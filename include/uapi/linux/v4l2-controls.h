0e0cc3070338af5dcf4d34efcfd6b6c3d2ffccc0
Nguyen Van Linh [FGA.BU13] <LinhNV25@fsoft.com.vn>
2018-04-10 09:26:31 +0700

    media: v4l2-ctrl: Add CID base for r8a7747x
    Signed-off-by: nhannguyen <nhan.nguyen.yb@renesas.com>
    Signed-off-by: Hao Bui <hao.bui.yg@renesas.com>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -178,6 +178,10 @@ enum v4l2_colorfx {
  * We reserve 16 controls for this driver. */
 #define V4L2_CID_USER_TC358743_BASE		(V4L2_CID_USER_BASE + 0x1080)
 
+/* The base for the r8a7747x driver controls.
+ * We reserve 16 controls for this driver. */
+#define V4L2_CID_USER_R8A7747X_BASE		(V4L2_CID_USER_BASE + 0x1090)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
