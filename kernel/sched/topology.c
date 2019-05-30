88331a52cd00e7c881dab916001ce2d6b088311b
Hien Huynh <hien.huynh.px@renesas.com>
2019-05-30 14:10:05 +0700

    sched: Add static_key for asymmetric cpu capacity optimizations
    The existing asymmetric cpu capacity code should cause minimal
    overhead for others. Putting it behind a static_key, it has been
    done for SMT optimizations, would make it easier to extend and
    improve without causing harm to others moving forward.
    
    Cherry-picked from https://patchwork.kernel.org/patch/10506515/
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Gaku Inami <gaku.inami.xh@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -399,6 +399,7 @@ DEFINE_PER_CPU(int, sd_llc_id);
 DEFINE_PER_CPU(struct sched_domain_shared *, sd_llc_shared);
 DEFINE_PER_CPU(struct sched_domain *, sd_numa);
 DEFINE_PER_CPU(struct sched_domain *, sd_asym);
+DEFINE_STATIC_KEY_FALSE(sched_asym_cpucapacity);
 
 static void update_top_cache_domain(int cpu)
 {
@@ -425,6 +426,22 @@ static void update_top_cache_domain(int cpu)
 	sd = highest_flag_domain(cpu, SD_ASYM_PACKING);
 	rcu_assign_pointer(per_cpu(sd_asym, cpu), sd);
 }
+
+static void update_asym_cpucapacity(int cpu)
+{
+	int enable = false;
+
+	rcu_read_lock();
+	if (lowest_flag_domain(cpu, SD_ASYM_CPUCAPACITY))
+		enable = true;
+	rcu_read_unlock();
+
+	if (enable) {
+		/* This expects to be hotplug-safe */
+		static_branch_enable_cpuslocked(&sched_asym_cpucapacity);
+	}
+}
+
 
 /*
  * Attach the domain 'sd' to 'cpu' as its base domain. Callers must
@@ -1704,6 +1721,10 @@ build_sched_domains(const struct cpumask *cpu_map, struct sched_domain_attr *att
 		cpu_attach_domain(sd, d.rd, i);
 	}
 	rcu_read_unlock();
+
+	if (!cpumask_empty(cpu_map))
+		update_asym_cpucapacity(cpumask_first(cpu_map));
+
 
 	if (rq && sched_debug_enabled) {
 		pr_info("root domain span: %*pbl (max cpu_capacity = %lu)\n",
