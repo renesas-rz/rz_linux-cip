72ccde95fd9d9965c04454e537ca437ad050388e
Hien Huynh <hien.huynh.px@renesas.com>
2019-09-19 11:30:43 +0700

    media: rcar-vin: Add UDS function support
    Add UDS (Up Down Scale) control for R-Car Gen3.
    UDS can be vertical and horizontal scaling.
    
    There is a possibility that scaling may not be executed
    when VUP bit in VnMC register is active and UDS relation registers
    can be not set. Therefore, Add processing to set VUP bit to 0 at
    the capture stop.
    
    In addition, before stopping the capture, if UDS (Up Down Scale) is set
    to disable and waiting for the capture completion, capture data invades
    to the area beyond the area allocated by dma_alloc function.
    Since it was obtained by the view of the H/W team that it is better to
    disable UDS after stopping the capture.
    
    Apply this patch support for RZ/G2 SoC too.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>
    (cherry pick commit 803f2c8ccae6b52f0dbcb5428d996774d184d935)
    from repository horms/renesas-bsp
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -92,6 +92,20 @@ struct rvin_parallel_entity {
 };
 
 /**
+ * struct rvin_uds_regs - UDS register information
+ * @ctrl:              UDS Control register
+ * @scale:             UDS Scaling Factor register
+ * @pass_bwidth:       UDS Passband Register
+ * @clip_size:         UDS Output Size Clipping Register
+ */
+struct rvin_uds_regs {
+	unsigned long ctrl;
+	unsigned long scale;
+	unsigned long pass_bwidth;
+	unsigned long clip_size;
+};
+
+/**
  * struct rvin_group_route - describes a route from a channel of a
  *	CSI-2 receiver to a VIN
  *
