062999fb8ee50a67a63d00537dc884d1b5d5fcef
Hien Huynh <hien.huynh.px@renesas.com>
2020-03-27 17:01:59 +0700

    soc: renesas: rcar-topology: Add support for RZ/G capacity awareness
    Base on commit "soc: renesas: rcar-topology: Add support for R-Car
    capacity awareness" with detailed description:
    
    Some R-Car SoCs support big LITTLE architecture produced by ARM.
    In order to keep all CPUs support on big LITTLE architecture,
    this patch changes the sched domain flags that the tasks can be
    scheduled with capacity awareness. If you use big LITTLE without
    this patch, the CFS scheduler may make unintended behaviors.
    
    This patch applies for R8A774A1 SoC.
    
    Signed-off-by: Gaku Inami <gaku.inami.xw@bp.renesas.com>
    Signed-off-by: Takeshi Kihara <takeshi.kihara.df@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

---
+++ drivers/soc/renesas/rcar-topology.c
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ *  R-Car CPU topology for ARM big.LITTLE platforms
+ *
+ * Copyright (C) 2017 Renesas Electronics Corporation.
+ */
+
+#include <linux/cpuset.h>
+#include <linux/init.h>
+#include <linux/of.h>
+#include <linux/sched/topology.h>
+#include <linux/topology.h>
+
+static int rcar_cpu_cpu_flags(void)
+{
+	return SD_ASYM_CPUCAPACITY;
+}
+
+static struct sched_domain_topology_level rcar_topology[] = {
+#ifdef CONFIG_SCHED_MC
+	{ cpu_coregroup_mask, cpu_core_flags, SD_INIT_NAME(MC) },
+#endif
+	{ cpu_cpu_mask, rcar_cpu_cpu_flags, SD_INIT_NAME(DIE) },
+	{ NULL, }
+};
+
+static int __init rcar_topology_init(void)
+{
+	if (of_machine_is_compatible("renesas,r8a7795") ||
+		of_machine_is_compatible("renesas,r8a7796") ||
+		of_machine_is_compatible("renesas,r8a774a1")) {
+		set_sched_topology(rcar_topology);
+	}
+
+	return 0;
+}
+early_initcall(rcar_topology_init);
