1881283e16dea5e922b18197348f18f117d4e8fa
Hien Huynh <hien.huynh.px@renesas.com>
2020-07-09 15:47:30 +0700

    clk: renesas: r8a774a1: Add R8A774A3 CPG/MSSR support
    Refer upstream commit id <2ba738d56db4ddb1c17e418cb501d303a8b481d2>
    support for R-Car M3-W+ and apply it for RZ/G2M v3.0.
    
    Add support for the RZ/G2M v3.0 (R8A774A3) SoC to the Renesas Clock
    Pulse Generator / Module Standby and Software Reset driver.
    
    RZ/G2M v3.0 is very similar to RZ/G2M v1.0 (R8A774A1), which allows for
    both SoCs to share a driver. RZ/G2M v3.0 lacks a few modules, so their
    clocks must be nullified.
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- drivers/clk/renesas/renesas-cpg-mssr.c
+++ drivers/clk/renesas/renesas-cpg-mssr.c
@@ -6,7 +6,7 @@
  * Based on clk-mstp.c, clk-rcar-gen2.c, and clk-rcar-gen3.c
  *
  * Copyright (C) 2013 Ideas On Board SPRL
- * Copyright (C) 2015 Renesas Electronics Corp.
+ * Copyright (C) 2015-2020 Renesas Electronics Corp.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -675,6 +675,12 @@ static const struct of_device_id cpg_mssr_match[] = {
 #ifdef CONFIG_CLK_R8A774A1
 	{
 		.compatible = "renesas,r8a774a1-cpg-mssr",
+		.data = &r8a774a1_cpg_mssr_info,
+	},
+#endif
+#ifdef CONFIG_CLK_R8A774A3
+	{
+		.compatible = "renesas,r8a774a3-cpg-mssr",
 		.data = &r8a774a1_cpg_mssr_info,
 	},
 #endif
