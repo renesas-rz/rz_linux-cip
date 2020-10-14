a2eb99b3b296a67a7f2fc54911a6f0669612fb02
thanhbui <thanh.bui.fz@renesas.com>
2020-10-14 13:07:24 +0700

    net: ethernet: sh_eth: Add R8A7743 support
    Add support for R8A7743 (RZ/G1M) SoC.
    
    Signed-off-by: thanhbui <thanh.bui.fz@renesas.com>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -3346,6 +3346,7 @@ static struct platform_device_id sh_eth_id_table[] = {
 	{ "r7s72100-ether", (kernel_ulong_t)&r7s72100_data },
 	{ "r8a7740-gether", (kernel_ulong_t)&r8a7740_data },
 	{ "r8a7742-ether", (kernel_ulong_t)&r8a779x_data },
+	{ "r8a7743-ether", (kernel_ulong_t)&r8a779x_data },
 	{ "r8a7744-ether", (kernel_ulong_t)&r8a779x_data },
 	{ "r8a777x-ether", (kernel_ulong_t)&r8a777x_data },
 	{ "r8a7790-ether", (kernel_ulong_t)&r8a779x_data },
