c3ba6adca7f6ebe07f03c4656a835284ad4df63f
Binh Nguyen <binh.nguyen.jz@renesas.com>
2021-05-25 16:47:42 +0700

    drm: rcar-du: rzg2l_mipi_dsi: Add common compatible string for RZ/G2L group
    Add a common compatible string for RZ/G2L group and remove the SoC
    specific string.
    
    Signed-off-by: Binh Nguyen <binh.nguyen.jz@renesas.com>

--- drivers/gpu/drm/rcar-du/rzg2l_mipi_dsi.c
+++ drivers/gpu/drm/rcar-du/rzg2l_mipi_dsi.c
@@ -938,7 +938,7 @@ static int rzg2l_mipi_dsi_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id rzg2l_mipi_dsi_of_table[] = {
-	{ .compatible = "renesas,r9a07g044l-mipi-dsi" },
+	{ .compatible = "renesas,rzg2l-mipi-dsi" },
 	{ },
 };
 
