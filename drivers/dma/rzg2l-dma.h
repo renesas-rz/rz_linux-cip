ef338cd1723cca24fcd2ba441bb96420b80c2007
Duy Bao Dang <duy.dang.yb@renesas.com>
2021-06-03 15:58:02 +0700

    dma: rzg2l-dma: Add new dma driver for RZG2L
    This patch adds new rzg2l-dma driver based on
    rza-dma driver.
    
    Signed-off-by: Duy Bao Dang <duy.dang.yb@renesas.com>

---
+++ drivers/dma/rzg2l-dma.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Renesas RZ/G2L DMA Controller Header
+ *
+ * Copyright (C) 2020 Renesas Electronics Corp.
+ */
+
+#ifndef _RZG2L_DMA_H
+#define _RZG2L_DMA_H
+
+#include <linux/types.h>
+#include <linux/kernel.h>
+
+struct rzg2l_dma_conf {
+	const struct dmac_mod	*mod_dmac;
+	unsigned int    	nids;
+};
+
+struct dmac_mod {
+        const unsigned int slave_id;
+        const u32 addr_slave;
+        const unsigned int chcfg;
+        const unsigned int dmars;
+};
+
+#endif
