cde1b45d08ec3351829c22dd30a8b02421382639
Hoa Huynh <hoa.huynh.ud@renesas.com>
2021-06-04 17:13:10 +0700

    spi: spi-rspi: Add RSPI slave mode.
    This patch adds RSPI slave mode.
    
    Signed-off-by: Hoa Huynh <hoa.huynh.ud@renesas.com>

--- include/linux/spi/rspi.h
+++ include/linux/spi/rspi.h
@@ -16,10 +16,16 @@
 #ifndef __LINUX_SPI_RENESAS_SPI_H__
 #define __LINUX_SPI_RENESAS_SPI_H__
 
+enum {
+	RSPI_SPI_MASTER,
+	RSPI_SPI_SLAVE,
+};
+
 struct rspi_plat_data {
 	unsigned int dma_tx_id;
 	unsigned int dma_rx_id;
 
+	u8 mode;
 	u16 num_chipselect;
 };
 
