57a41a57872e9fb0e003c22aab4e4f4eae5afa61
Boris Brezillon <boris.brezillon@bootlin.com>
2021-07-07 16:26:11 +0700

    mtd: spi-nor: Add the SNOR_F_4B_OPCODES flag
    Some flash_info entries have the SPI_NOR_4B_OPCODES flag set to let the
    core know that the flash supports 4B opcode. While this solution works
    fine for id-based caps detection, it doesn't work that well when relying
    on SFDP-based caps detection. Let's add an SNOR_F_4B_OPCODES flag so
    that the SFDP parsing code can set it when appropriate.
    
    Signed-off-by: Boris Brezillon <boris.brezillon@bootlin.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    
    Signed-off-by: Quynh Nguyen <quynh.nguyen.xb@renesas.com>

--- include/linux/mtd/spi-nor.h
+++ include/linux/mtd/spi-nor.h
@@ -236,6 +236,7 @@ enum spi_nor_option_flags {
 	SNOR_F_READY_XSR_RDY	= BIT(4),
 	SNOR_F_USE_CLSR		= BIT(5),
 	SNOR_F_BROKEN_RESET	= BIT(6),
+	SNOR_F_4B_OPCODES	= BIT(7),
 };
 
 /**
@@ -417,6 +418,18 @@ static u64 __maybe_unused
 spi_nor_region_end(const struct spi_nor_erase_region *region)
 {
 	return (region->offset & ~SNOR_ERASE_FLAGS_MASK) + region->size;
+}
+
+static void __maybe_unused
+spi_nor_region_mark_end(struct spi_nor_erase_region *region)
+{
+	region->offset |= SNOR_LAST_REGION;
+}
+
+static void __maybe_unused
+spi_nor_region_mark_overlay(struct spi_nor_erase_region *region)
+{
+	region->offset |= SNOR_OVERLAID_REGION;
 }
 
 static bool __maybe_unused spi_nor_has_uniform_erase(const struct spi_nor *nor)
