a9ab76e56d1bbb30590d100c38e537d6182a09b8
Hien Huynh <hien.huynh.px@renesas.com>
2021-07-28 09:12:54 +0700

    pinctrl: pfc-r9a07g044c: add pinctrl support for R9A07G044C SoC
    This commits add pinctrl support for R9407G044C SoC.
    Currently, it uses same pin definition with R9A07G044L but fewer pins.
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>

--- drivers/pinctrl/sh-pfc/pinctrl-rzg2l.c
+++ drivers/pinctrl/sh-pfc/pinctrl-rzg2l.c
@@ -1078,6 +1078,12 @@ static const struct of_device_id rzg2l_pinctrl_of_table[] = {
 		.data = &r9a07g044l_pinctrl_data,
 	},
 #endif
+#ifdef CONFIG_PINCTRL_R9A07G044C
+	{
+		.compatible = "renesas,r9a07g044c-pinctrl",
+		.data = &r9a07g044c_pinctrl_data,
+	},
+#endif
 	{},
 };
 
