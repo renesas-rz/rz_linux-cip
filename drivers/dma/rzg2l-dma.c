5e3947eaa9df00600d56e237603a74fa5119be81
Binh Nguyen <binh.nguyen.jz@renesas.com>
2021-08-02 04:57:34 +0700

    dma: rzg2l-dma: add support for RZ/G2LC
    Add support for RZ/G2LC (R9A07G044C) similar to R9A07G044L.
    
    Signed-off-by: Binh Nguyen <binh.nguyen.jz@renesas.com>

--- drivers/dma/rzg2l-dma.c
+++ drivers/dma/rzg2l-dma.c
@@ -93,7 +93,8 @@ union dmars_reg {
 	u32 v;
 	struct {
 		u32 rid:   2;	/* LSB */
-#if defined(CONFIG_ARCH_R9A07G044L2) || defined(CONFIG_ARCH_R9A07G054L2)
+#if defined(CONFIG_ARCH_R9A07G044L2) || defined(CONFIG_ARCH_R9A07G054L2) \
+				     || defined(CONFIG_ARCH_R9A07G044C2)
 		u32 mid:   8;
 		u32 _mbz0:22;
 #else
@@ -1230,6 +1231,12 @@ static const struct of_device_id of_rzg2ldma_match[] = {
 #ifdef CONFIG_R9A07G044L_DMA
 	{
 		.compatible = "renesas,r9a07g044l-dma",
+		.data = &r9a07g044l_dma_config,
+	},
+#endif
+#ifdef CONFIG_R9A07G044C_DMA
+	{
+		.compatible = "renesas,r9a07g044c-dma",
 		.data = &r9a07g044l_dma_config,
 	},
 #endif
