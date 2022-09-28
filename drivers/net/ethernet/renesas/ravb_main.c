renesas-rcar/linux-bsp

net: renesas: ravb: Fix a stuck issue when a lot of frames are received
Since the previous code always cleared the interrupt flag of RX but
checks the interrupt flags in ravb_poll(), ravb_poll() could not
call ravb_rx() in the next time until a new RX frame was received if
ravb_rx() returned true. For example, the following command could
cause this issue.

    $ sudo ping -f -l 1000 -c 1000 <this driver's ipaddress>

To fix the issue, always calls ravb_rx() regardless the interrupt
flags condition.

Fixes: c156633f1353 ("Renesas Ethernet AVB driver proper")
Signed-off-by: Yoshihiro Shimoda <yoshihiro.shimoda.uh@renesas.com>
Signed-off-by: Tho Vu <tho.vu.wh@renesas.com>

drivers/net/ethernet/renesas/ravb_main.c
@@ -885,36 +885,23 @@ static int ravb_poll(struct napi_struct *napi, int budget)
 	int q = napi - priv->napi;
 	int mask = BIT(q);
 	int quota = budget;
-	u32 ris0, tis;
 
-	for (;;) {
-		tis = ravb_read(ndev, TIS);
-		ris0 = ravb_read(ndev, RIS0);
-		if (!((ris0 & mask) || (tis & mask)))
-			break;
+	/* Clear RX interrupt */
+	ravb_write(ndev, ~(mask | RIS0_RESERVED_BIT), RIS0);
+	if (ravb_rx(ndev, &quota, q))
+		goto out;
 
-		/* Processing RX Descriptor Ring */
-		if (ris0 & mask) {
-			/* Clear RX interrupt */
-			ravb_write(ndev, ~(mask | RIS0_RESERVED_BIT), RIS0);
-			if (ravb_rx(ndev, &quota, q))
-				goto out;
-		}
-		/* Processing TX Descriptor Ring */
-		if (tis & mask) {
-			spin_lock_irqsave(&priv->lock, flags);
-			/* Timestamp updated */
-			if (q == RAVB_NC)
-				ravb_get_tx_tstamp(ndev);
+	spin_lock_irqsave(&priv->lock, flags);
+	/* Timestamp updated */
+	if ((q == RAVB_NC) && (ravb_read(ndev, TIS) & mask))
+		ravb_get_tx_tstamp(ndev);
 
-			/* Clear TX interrupt */
-			ravb_write(ndev, ~(mask | TIS_RESERVED_BIT), TIS);
-			ravb_tx_free(ndev, q, true);
-			netif_wake_subqueue(ndev, q);
-			mmiowb();
-			spin_unlock_irqrestore(&priv->lock, flags);
-		}
-	}
+	/* Clear TX interrupt */
+	ravb_write(ndev, ~(mask | TIS_RESERVED_BIT), TIS);
+	ravb_tx_free(ndev, q, true);
+	netif_wake_subqueue(ndev, q);
+	mmiowb();
+	spin_unlock_irqrestore(&priv->lock, flags);
 
 	napi_complete(napi);
 

renesas-rz/rz_linux-cip

Signed-off-by: Loc Pham <loc.pham.xh@renesas.com>

drivers/net/ethernet/renesas/ravb_main.c
@@ -904,36 +904,23 @@ static int ravb_poll(struct napi_struct *napi, int budget)
 	int q = napi - priv->napi;
 	int mask = BIT(q);
 	int quota = budget;
-	u32 ris0, tis;
 
-	for (;;) {
-		tis = ravb_read(ndev, TIS);
-		ris0 = ravb_read(ndev, RIS0);
-		if (!((ris0 & mask) || (tis & mask)))
-			break;
+	/* Clear RX interrupt */
+	ravb_write(ndev, ~(mask | RIS0_RESERVED), RIS0);
+	if (ravb_rx(ndev, &quota, q))
+		goto out;
 
-		/* Processing RX Descriptor Ring */
-		if (ris0 & mask) {
-			/* Clear RX interrupt */
-			ravb_write(ndev, ~(mask | RIS0_RESERVED), RIS0);
-			if (ravb_rx(ndev, &quota, q))
-				goto out;
-		}
-		/* Processing TX Descriptor Ring */
-		if (tis & mask) {
-			spin_lock_irqsave(&priv->lock, flags);
-			/* Timestamp updated */
-			if (q == RAVB_NC)
-				ravb_get_tx_tstamp(ndev);
+	spin_lock_irqsave(&priv->lock, flags);
+	/* Timestamp updated */
+	if ((q == RAVB_NC) && (ravb_read(ndev, TIS) & mask))
+		ravb_get_tx_tstamp(ndev);
 
-			/* Clear TX interrupt */
-			ravb_write(ndev, ~(mask | TIS_RESERVED), TIS);
-			ravb_tx_free(ndev, q, true);
-			netif_wake_subqueue(ndev, q);
-			mmiowb();
-			spin_unlock_irqrestore(&priv->lock, flags);
-		}
-	}
+	/* Clear TX interrupt */
+	ravb_write(ndev, ~(mask | TIS_RESERVED), TIS);
+	ravb_tx_free(ndev, q, true);
+	netif_wake_subqueue(ndev, q);
+	mmiowb();
+	spin_unlock_irqrestore(&priv->lock, flags);
 
 	napi_complete(napi);
 