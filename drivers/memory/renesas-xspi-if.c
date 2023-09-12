8431f2575db12ec64f5f299fb03ded3d3cac850a
hienhuynh <hien.huynh.px@renesas.com>
2023-09-12 19:20:00 +0700

    memory: renesas-xspi-if: update set/get flow for xSPI Command Manual Data Buf
    xSPI can support 2 xSPI Command Manual Data Buf Registers to get or set
    data in manual/command mode.
    It can support upto 8 bytes data in transaction.
    So update manual transfer code to set/get data in 2 cases:
    - If number of data is higher than 4 bytes, 2 Buf registers will be
    used.
    - If number of data is equal to or smaller than 4 bytes, only Buf 0
    register will be used.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Tranh Ha <tranh.ha.xb@renesas.com>

--- drivers/memory/renesas-xspi-if.c
+++ drivers/memory/renesas-xspi-if.c
@@ -442,7 +442,7 @@ EXPORT_SYMBOL(xspi_prepare);
 
 int xspi_manual_xfer(struct rpcif *xspi)
 {
-	u32 pos = 0, max = xspi->bus_size == 2 ? 8 : 4;
+	u32 pos = 0, max = 8;
 	int ret = 0;
 
 	pm_runtime_get_sync(xspi->dev);
@@ -477,8 +477,7 @@ int xspi_manual_xfer(struct rpcif *xspi)
 			regmap_update_bits(xspi->regmap, XSPI_CDTBUF0,
 					XSPI_CDTBUF_TRTYPE, XSPI_CDTBUF_TRTYPE);
 
-			/* nbytes may only be 1, 2, 4, or 8 */
-			nbytes = bytes_left >= max ? max : (1 << ilog2(bytes_left));
+			nbytes = bytes_left >= max ? max : bytes_left;
 
 			regmap_update_bits(xspi->regmap, XSPI_CDTBUF0,
 					XSPI_CDTBUF_DATASIZE(0xf),
@@ -491,7 +490,17 @@ int xspi_manual_xfer(struct rpcif *xspi)
 			xspi->xfer_size = nbytes;
 
 			memcpy(data, xspi->buffer + pos, nbytes);
-			regmap_write(xspi->regmap, XSPI_CDD0BUF0, *p);
+
+			if (nbytes > 4) {
+				xspi->xfer_size = 4;
+				regmap_write(xspi->regmap, XSPI_CDD0BUF0, *p++);
+				xspi->xfer_size = 1 << ilog2(nbytes - 4);
+				regmap_write(xspi->regmap, XSPI_CDD1BUF0, *p);
+			} else {
+				xspi->xfer_size = 1 << ilog2(bytes_left);
+				regmap_write(xspi->regmap, XSPI_CDD0BUF0, *p);
+			}
+
 			regmap_write(xspi->regmap, XSPI_CDABUF0, xspi->smadr + pos);
 
 			regmap_update_bits(xspi->regmap, XSPI_CDCTL0,
@@ -517,8 +526,8 @@ int xspi_manual_xfer(struct rpcif *xspi)
 			regmap_update_bits(xspi->regmap, XSPI_CDTBUF0,
 					XSPI_CDTBUF_TRTYPE, ~(u32)XSPI_CDTBUF_TRTYPE);
 
-			/* nbytes may only be 1, 2, 4, or 8 */
-			nbytes = bytes_left >= max ? max : (1 << ilog2(bytes_left));
+			/* nbytes can be up to 8 bytes */
+			nbytes = bytes_left >= max ? max : bytes_left;
 
 			regmap_update_bits(xspi->regmap, XSPI_CDTBUF0,
 					XSPI_CDTBUF_DATASIZE(0xf),
@@ -528,7 +537,6 @@ int xspi_manual_xfer(struct rpcif *xspi)
 					XSPI_CDTBUF_ADDSIZE(0x7),
 					XSPI_CDTBUF_ADDSIZE(xspi->addr_nbytes));
 
-			xspi->xfer_size = nbytes;
 
 			if (xspi->addr_nbytes)
 				regmap_write(xspi->regmap, XSPI_CDABUF0, xspi->smadr + pos);
@@ -544,7 +552,16 @@ int xspi_manual_xfer(struct rpcif *xspi)
 			if (ret)
 				goto err_out;
 
-			regmap_read(xspi->regmap, XSPI_CDD0BUF0, p);
+			if (nbytes > 4) {
+				xspi->xfer_size = 4;
+				regmap_read(xspi->regmap, XSPI_CDD0BUF0, p++);
+				xspi->xfer_size = 1 << ilog2(nbytes - 4);
+				regmap_read(xspi->regmap, XSPI_CDD1BUF0, p);
+			} else {
+				xspi->xfer_size = 1 << ilog2(bytes_left);
+				regmap_read(xspi->regmap, XSPI_CDD0BUF0, p);
+			}
+
 			memcpy(xspi->buffer + pos, data, nbytes);
 
 			regmap_update_bits(xspi->regmap, XSPI_INTC,
