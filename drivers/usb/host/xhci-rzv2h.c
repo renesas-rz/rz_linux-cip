c54b2f4bbe127ce04516f5c0e639a276fecdbcee
Minh Nguyen <minh.nguyen.zj@renesas.com>
2023-12-12 14:07:31 +0700

    drivers: usb: host: xhci-rzv2h: Support USB3 SuperSpeed in V2H board.
    This patch is to support USB3 SuperSpeed for both V2H boards.
    
    Signed-off-by: Minh Nguyen <minh.nguyen.zj@renesas.com>

--- drivers/usb/host/xhci-rzv2h.c
+++ drivers/usb/host/xhci-rzv2h.c
@@ -29,6 +29,7 @@
 #define USB2TEST_RESET				0x0
 #define USB2TEST_OTGR				0x600
 #define USB2TEST_UTMICTRL2			0xB04
+#define USB2TEST_PRMCTRL5			0xC0C
 
 /* USB3TEST Registers */
 #define USB3TEST_RSTCTRL			0x1000
@@ -36,8 +37,127 @@
 #define USB3TEST_RAMCTRL			0x100C
 #define USB3TEST_RAMCTRL_SRAM_INIT_DONE		BIT(2)
 #define USB3TEST_CREGCTRL			0x1010
+#define USB3TEST_CREGCTRL_ENTRY                 BIT(0)
+#define USB3TEST_CREGCTRL_READ                  BIT(2)
+#define USB3TEST_CREGCTRL_WRITE                 BIT(3)
+#define USB3TEST_CREGCTRL_BUSY                  BIT(4)
+#define USB3TEST_CREGCTRL_MASK                  0xFFFFFFE2
 #define USB3TEST_CREGCTRL_PARA_SEL		BIT(8)
 #define USB3TEST_LANECONFIG0			0x1030
+#define USB3TEST_CREGADDRESS			0x1014
+#define USB3TEST_CREGWRITEDATA			0x1018
+#define USB3TEST_CREGREADDATA			0x101C
+
+static int __maybe_unused usb30_phy_read(void __iomem *usbtest, u16 phy_adrs)
+{
+	u32 data;
+	u8 timeout = 100;
+
+	writel(USB3TEST_CREGCTRL_PARA_SEL, usbtest + USB3TEST_CREGCTRL);
+	writel(phy_adrs, usbtest + USB3TEST_CREGADDRESS);
+
+	data = readl(usbtest + USB3TEST_CREGCTRL);
+	data = (data & USB3TEST_CREGCTRL_MASK) | USB3TEST_CREGCTRL_READ | USB3TEST_CREGCTRL_ENTRY;
+	writel(data, usbtest + USB3TEST_CREGCTRL);
+
+	while (timeout--) {
+		if (readl(usbtest + USB3TEST_CREGCTRL) & USB3TEST_CREGCTRL_BUSY)
+			break;
+
+		cpu_relax();
+	};
+
+	if (!timeout)
+		goto err;
+
+	/* Reset timeout */
+	timeout = 100;
+
+	writel(data & ~USB3TEST_CREGCTRL_ENTRY, usbtest + USB3TEST_CREGCTRL);
+
+	while (timeout--) {
+		if ((readl(usbtest + USB3TEST_CREGCTRL) & USB3TEST_CREGCTRL_BUSY) == 0)
+			break;
+
+		cpu_relax();
+	};
+
+err:
+	if (!timeout)
+		return -ETIMEDOUT;
+
+	return readl(usbtest + USB3TEST_CREGREADDATA);
+}
+
+static int  __maybe_unused usb30_phy_write(void __iomem *usbtest, u16 phy_adrs, u32 phy_wdata)
+{
+	u32 data;
+	u8 timeout = 100;
+
+	writel(USB3TEST_CREGCTRL_PARA_SEL, usbtest + USB3TEST_CREGCTRL);
+	writel(phy_adrs, usbtest + USB3TEST_CREGADDRESS);
+	writel(phy_wdata, usbtest + USB3TEST_CREGWRITEDATA);
+
+	data = readl(usbtest + USB3TEST_CREGCTRL);
+	data = (data & USB3TEST_CREGCTRL_MASK) | USB3TEST_CREGCTRL_WRITE | USB3TEST_CREGCTRL_ENTRY;
+	writel(data, usbtest + USB3TEST_CREGCTRL);
+
+	while (timeout--) {
+		if (readl(usbtest + USB3TEST_CREGCTRL) & USB3TEST_CREGCTRL_BUSY)
+			break;
+
+		cpu_relax();
+	};
+
+	if (!timeout)
+		goto err;
+
+	/* Reset timeout */
+	timeout = 100;
+
+	writel(data & ~USB3TEST_CREGCTRL_ENTRY, usbtest + USB3TEST_CREGCTRL);
+
+	while (timeout--) {
+		if ((readl(usbtest + USB3TEST_CREGCTRL) & USB3TEST_CREGCTRL_BUSY) == 0)
+			break;
+
+		cpu_relax();
+	};
+
+err:
+	if (!timeout)
+		return -ETIMEDOUT;
+
+	return 0;
+}
+
+static int __maybe_unused usb30_phy_write_mon(void __iomem *usbtest, u16 phy_adrs, u32 phy_wdata)
+{
+	u32 data;
+	u8 timeout = 100;
+
+	pr_info("before : 0x%04x, 0x%04x", phy_adrs, usb30_phy_read(usbtest, phy_adrs));
+
+	writel(USB3TEST_CREGCTRL_PARA_SEL, usbtest + USB3TEST_CREGCTRL);
+	writel(phy_adrs, usbtest + USB3TEST_CREGADDRESS);
+	writel(phy_wdata, usbtest + USB3TEST_CREGWRITEDATA);
+
+	data = readl(usbtest + USB3TEST_CREGCTRL);
+	data = (data & USB3TEST_CREGCTRL_MASK) | USB3TEST_CREGCTRL_WRITE | USB3TEST_CREGCTRL_ENTRY;
+	writel(data, usbtest + USB3TEST_CREGCTRL);
+
+	while (timeout--) {
+		if ((readl(usbtest + USB3TEST_CREGCTRL) & USB3TEST_CREGCTRL_BUSY) == 0)
+			break;
+
+		cpu_relax();
+	};
+
+	if (!timeout)
+		return -ETIMEDOUT;
+
+	return 0;
+}
 
 void xhci_rzv2h_start(struct usb_hcd *hcd)
 {
@@ -54,11 +174,23 @@ void xhci_rzv2h_start(struct usb_hcd *hcd)
 void usb2test_phy_init(void __iomem *usbtest)
 {
 	writel(0x00000303, usbtest + USB2TEST_UTMICTRL2);
+	writel(0x0000020E, usbtest + USB2TEST_RESET);
+	udelay(10);
+
 	writel(0x00000206, usbtest + USB2TEST_RESET);
+	writel(0x00000140, usbtest + USB2TEST_PRMCTRL5);
+	udelay(10);
+
+	writel(0x00000202, usbtest + USB2TEST_RESET);
+	udelay(10);
+
 	writel(0x00000200, usbtest + USB2TEST_RESET);
-	writel(0x00000003, usbtest + USB2TEST_UTMICTRL2);
+	writel(0x00000300, usbtest + USB2TEST_UTMICTRL2);
+	udelay(10);
+	writel(0x00000000, usbtest + USB2TEST_UTMICTRL2);
+
 	writel(0x00000000, usbtest + USB2TEST_RESET);
-	writel(0x00000909, usbtest + USB2TEST_OTGR);
+	writel(0x00000101, usbtest + USB2TEST_OTGR);
 }
 
 void usb3test_phy_init(void __iomem *usbtest)
@@ -66,10 +198,10 @@ void usb3test_phy_init(void __iomem *usbtest)
 	writel(USB3TEST_CREGCTRL_PARA_SEL, usbtest + USB3TEST_CREGCTRL);
 	writel(0x00000303, usbtest +  USB3TEST_RSTCTRL);
 
-	udelay(10);
+	udelay(20);
 
-	writel(0x00000000, usbtest + USB3TEST_CLKCTRL);
-	writel(0x0000000B, usbtest + USB3TEST_LANECONFIG0);
+	writel(0x00000004, usbtest + USB3TEST_CLKCTRL);
+	writel(0x0000000D, usbtest + USB3TEST_LANECONFIG0);
 	writel(0x00000301, usbtest + USB3TEST_RSTCTRL);
 
 	while (1) {
@@ -81,8 +213,8 @@ void usb3test_phy_init(void __iomem *usbtest)
 	};
 
 	writel(0x00000300, usbtest +  USB3TEST_RSTCTRL);
-	writel(0x00000001, usbtest +  USB3TEST_RSTCTRL);
-	writel(0x00000300, usbtest +  USB3TEST_RSTCTRL);
+	writel(0x00000001, usbtest +  USB3TEST_RAMCTRL);
+	writel(0x00000000, usbtest +  USB3TEST_RSTCTRL);
 }
 
 int xhci_rzv2h_setup(struct usb_hcd *hcd)
