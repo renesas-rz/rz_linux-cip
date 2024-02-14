c9034a5e4a1f886df2fd50f70de044a55be977de
Quynh Nguyen <quynh.nguyen.xb@renesas.com>
2024-02-14 10:30:49 +0100

    pwm: Provide wrappers for storing and getting driver private data
    These functions are useful to store and query driver private data
    depending on the pwm_chip. After struct pwm_chip got its own struct
    device, this can make use of dev_get_drvdata() and dev_set_drvdata() on
    that device. These functions are required already now to convert
    drivers to pwmchip_alloc() which must happen before changing
    pwm_chip::dev.
    
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Link: https://lore.kernel.org/r/67514cdf29d29bd8b4ad8d44fac87f6ae6dca1e5.1707900770.git.u.kleine-koenig@pengutronix.de
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Y Pham <y.pham.yg@bp.renesas.com>
    Signel-off-by: Quynh Nguyen <quynh.nguyen.xb@renesas.com>

--- include/linux/pwm.h
+++ include/linux/pwm.h
@@ -290,6 +290,7 @@ struct pwm_ops {
  * @of_xlate: request a PWM device given a device tree PWM specifier
  * @of_pwm_n_cells: number of cells expected in the device tree PWM specifier
  * @list: list node for internal use
+ * @driver_data: Private pointer for driver specific info
  * @pwms: array of PWM devices allocated by the framework
  */
 struct pwm_chip {
@@ -304,8 +305,27 @@ struct pwm_chip {
 
 	/* only used internally by the PWM framework */
 	struct list_head list;
+	void *driver_data;
 	struct pwm_device *pwms;
 };
+
+static inline void *pwmchip_get_drvdata(struct pwm_chip *chip)
+{
+	/*
+	 * After pwm_chip got a dedicated struct device, this can be replaced by
+	 * dev_get_drvdata(&chip->dev);
+	 */
+	return chip->driver_data;
+}
+
+static inline void pwmchip_set_drvdata(struct pwm_chip *chip, void *data)
+{
+	/*
+	 * After pwm_chip got a dedicated struct device, this can be replaced by
+	 * dev_set_drvdata(&chip->dev, data);
+	 */
+	chip->driver_data = data;
+}
 
 #if IS_ENABLED(CONFIG_PWM)
 /* PWM user APIs */
