098c1344a38062d2667e316bb0b8da7c4027cabb
Hao Bui <hao.bui.yg@renesas.com>
2024-03-13 19:42:01 +0700

    clk: renesas: rzg2l: Fix PLLn rate calculation for RZ/G3S
    According to HW manual, PLLn clock rate must be within predefine ranges.
    This patch makes sure PLL recalc_rate function always return valid clock
    rate value which is closest to the acutual value.
    
    Signed-off-by: Hao Bui <hao.bui.yg@renesas.com>

--- drivers/clk/renesas/rzg2l-cpg.c
+++ drivers/clk/renesas/rzg2l-cpg.c
@@ -1068,10 +1068,26 @@ static unsigned long rzg3s_cpg_pll_clk_recalc_rate(struct clk_hw *hw,
 	struct pll_clk *pll_clk = to_pll(hw);
 	struct rzg2l_cpg_priv *priv = pll_clk->priv;
 	u32 nir, nfr, mr, pr, val, setting;
-	u64 rate;
+	u64 rate, min_rate, max_rate, actual_rate;
 
 	if (pll_clk->type != CLK_TYPE_G3S_PLL)
 		return parent_rate;
+	switch (GET_REG_SAMPLL_CLK1(pll_clk->conf)) {
+	case 0x04:			/* PLL1 */
+		max_rate = 1100000000;
+		min_rate = 1100000000;
+		break;
+	case 0x34:			/* PLL4 */
+		max_rate = 800000000;
+		min_rate = 400000000;
+		break;
+	case 0x54:			/* PLL6 */
+		max_rate = 500000000;
+		min_rate = 500000000;
+		break;
+	default:
+		return parent_rate;
+	};
 
 	setting = GET_REG_SAMPLL_SETTING(pll_clk->conf);
 	if (setting) {
@@ -1093,7 +1109,14 @@ static unsigned long rzg3s_cpg_pll_clk_recalc_rate(struct clk_hw *hw,
 
 	rate = mul_u64_u32_shr(parent_rate, 4096 * nir + nfr, 12);
 
-	return DIV_ROUND_CLOSEST_ULL(rate, (mr * pr));
+	actual_rate = DIV_ROUND_CLOSEST_ULL(rate, (mr * pr));
+
+	if (actual_rate < min_rate)
+		actual_rate = min_rate;
+	else if (actual_rate > max_rate)
+		actual_rate = max_rate;
+
+	return actual_rate;
 }
 
 static const struct clk_ops rzg3s_cpg_pll_ops = {
