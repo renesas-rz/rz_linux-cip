e7c0173cb7c59b3100a74f30be7d8619c3b5eaf3
Nghia Vo <nghia.vo.zn@renesas.com>
2024-10-10 14:32:30 +0700

    media: platform: rzg2l-cru: rzg2l-core: Use temporary variable for struct device in rzg2l_cru_probe()
    Use a temporary variable for the struct device pointers to avoid
    dereferencing.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Signed-off-by: Khai Nguyen <khai.nguyen.wx@renesas.com>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-core.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-core.c
@@ -276,33 +276,31 @@ static int rzg2l_cru_probe(struct platform_device *pdev)
 	struct v4l2_ctrl *ctrl;
 	int ret;
 
-	cru = devm_kzalloc(&pdev->dev, sizeof(*cru), GFP_KERNEL);
+	cru = devm_kzalloc(dev, sizeof(*cru), GFP_KERNEL);
 	if (!cru)
 		return -ENOMEM;
-
-	cru->info = of_device_get_match_data(dev);
 
 	cru->base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(cru->base))
 		return PTR_ERR(cru->base);
 
-	cru->presetn = devm_reset_control_get_shared(&pdev->dev, "presetn");
+	cru->presetn = devm_reset_control_get_shared(dev, "presetn");
 	if (IS_ERR(cru->presetn))
 		return dev_err_probe(&pdev->dev, PTR_ERR(cru->presetn),
 				     "Failed to get cpg presetn\n");
 
-	cru->aresetn = devm_reset_control_get_exclusive(&pdev->dev, "aresetn");
+	cru->aresetn = devm_reset_control_get_exclusive(dev, "aresetn");
 	if (IS_ERR(cru->aresetn))
 		return dev_err_probe(&pdev->dev, PTR_ERR(cru->aresetn),
 				     "Failed to get cpg aresetn\n");
 
-	cru->vclk = devm_clk_get(&pdev->dev, "video");
+	cru->vclk = devm_clk_get(dev, "video");
 	if (IS_ERR(cru->vclk))
-		return dev_err_probe(&pdev->dev, PTR_ERR(cru->vclk),
+		return dev_err_probe(dev, PTR_ERR(cru->vclk),
 				     "Failed to get video clock\n");
 
-	cru->dev = &pdev->dev;
-	cru->info = of_device_get_match_data(&pdev->dev);
+	cru->dev = dev;
+	cru->info = of_device_get_match_data(dev);
 
 	cru->image_conv_irq = platform_get_irq(pdev, 0);
 	if (cru->image_conv_irq < 0)
@@ -315,8 +313,8 @@ static int rzg2l_cru_probe(struct platform_device *pdev)
 		return ret;
 
 	cru->num_buf = RZG2L_CRU_HW_BUFFER_DEFAULT;
-	pm_suspend_ignore_children(&pdev->dev, true);
-	pm_runtime_enable(&pdev->dev);
+	pm_suspend_ignore_children(dev, true);
+	pm_runtime_enable(dev);
 
 	ret = rzg2l_cru_media_init(cru);
 	if (ret)
@@ -349,7 +347,7 @@ free_ctrl:
 
 error_dma_unregister:
 	rzg2l_cru_dma_unregister(cru);
-	pm_runtime_disable(&pdev->dev);
+	pm_runtime_disable(dev);
 
 	return ret;
 }
