16eee2bf9f36307917d85177fb35e3180514617b
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2024-10-19 14:16:00 +0300

    clk: Provide devm_clk_bulk_get_all_enabled() helper
    Commit 265b07df758a ("clk: Provide managed helper to get and enable bulk
    clocks") added devm_clk_bulk_get_all_enable() function, but missed to
    return the number of clocks stored in the clk_bulk_data table referenced
    by the clks argument.  Without knowing the number, it's not possible to
    iterate these clocks when needed, hence the argument is useless and
    could have been simply removed.
    
    Introduce devm_clk_bulk_get_all_enabled() variant, which is consistent
    with devm_clk_bulk_get_all() in terms of the returned value:
    
     > 0 if one or more clocks have been stored
     = 0 if there are no clocks
     < 0 if an error occurred
    
    Moreover, the naming is consistent with devm_clk_get_enabled(), i.e. use
    the past form of 'enable'.
    
    To reduce code duplication and improve patch readability, make
    devm_clk_bulk_get_all_enable() use the new helper, as suggested by
    Stephen Boyd.
    
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Reviewed-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/clk/clk-devres.c
+++ drivers/clk/clk-devres.c
@@ -221,6 +221,15 @@ static void devm_clk_bulk_release_all_enable(struct device *dev, void *res)
 int __must_check devm_clk_bulk_get_all_enable(struct device *dev,
 					      struct clk_bulk_data **clks)
 {
+	int ret = devm_clk_bulk_get_all_enabled(dev, clks);
+
+	return ret > 0 ? 0 : ret;
+}
+EXPORT_SYMBOL_GPL(devm_clk_bulk_get_all_enable);
+
+int __must_check devm_clk_bulk_get_all_enabled(struct device *dev,
+					       struct clk_bulk_data **clks)
+{
 	struct clk_bulk_devres *devres;
 	int ret;
 
@@ -230,25 +239,26 @@ int __must_check devm_clk_bulk_get_all_enable(struct device *dev,
 		return -ENOMEM;
 
 	ret = clk_bulk_get_all(dev, &devres->clks);
-	if (ret > 0) {
-		*clks = devres->clks;
-		devres->num_clks = ret;
-	} else {
+	if (ret <= 0) {
 		devres_free(devres);
 		return ret;
 	}
 
+	*clks = devres->clks;
+	devres->num_clks = ret;
+
 	ret = clk_bulk_prepare_enable(devres->num_clks, *clks);
-	if (!ret) {
-		devres_add(dev, devres);
-	} else {
+	if (ret) {
 		clk_bulk_put_all(devres->num_clks, devres->clks);
 		devres_free(devres);
+		return ret;
 	}
 
-	return ret;
+	devres_add(dev, devres);
+
+	return devres->num_clks;
 }
-EXPORT_SYMBOL_GPL(devm_clk_bulk_get_all_enable);
+EXPORT_SYMBOL_GPL(devm_clk_bulk_get_all_enabled);
 
 static int devm_clk_match(struct device *dev, void *res, void *data)
 {
