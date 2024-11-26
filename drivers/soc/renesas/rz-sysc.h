5cea29bbbd756c199e0de2afd3d52a7264108c1d
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-11-26 11:20:39 +0200

    soc: renesas: rz-sysc: Add SoC detection support
    The RZ SYSC controller has registers that keep the SoC ID data. Add
    driver support to retrieve the SoC ID and register a SoC driver.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Quynh Nguyen <quynh.nguyen.xb@renesas.com>

--- drivers/soc/renesas/rz-sysc.h
+++ drivers/soc/renesas/rz-sysc.h
@@ -36,12 +36,30 @@ struct rz_sysc_signal {
 };
 
 /**
+ * struct rz_syc_soc_id_init_data - RZ SYSC SoC identification initialization data
+ * @family: RZ SoC family
+ * @id: RZ SoC expected ID
+ * @devid_offset: SYSC SoC ID register offset
+ * @revision_mask: SYSC SoC ID revision mask
+ * @specific_id_mask: SYSC SoC ID specific ID mask
+ */
+struct rz_sysc_soc_id_init_data {
+	const char * const family;
+	u32 id;
+	u32 devid_offset;
+	u32 revision_mask;
+	u32 specific_id_mask;
+};
+
+/**
  * struct rz_sysc_init_data - RZ SYSC initialization data
+ * @soc_id_init_data: RZ SYSC SoC ID initialization data
  * @signals_init_data: RZ SYSC signals initialization data
  * @num_signals: number of SYSC signals
  * @max_register_offset: Maximum SYSC register offset to be used by the regmap config
  */
 struct rz_sysc_init_data {
+	const struct rz_sysc_soc_id_init_data *soc_id_init_data;
 	const struct rz_sysc_signal_init_data *signals_init_data;
 	u32 num_signals;
 	u32 max_register_offset;
