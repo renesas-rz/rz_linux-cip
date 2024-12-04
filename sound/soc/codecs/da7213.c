01b6e5fdb97803c2831c339bb834016c641c61cf
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-12-04 16:36:29 +0700

    ASoC: da7213: Populate max_register to regmap_config
    commit 9d4f9f6a7bb1afbde57d08c98f2db4ff019ee19d upstream.
    
    On the Renesas RZ/G3S SMARC Carrier II board having a DA7212 codec (using
    da7213 driver) connected to one SSIF-2 available on the Renesas RZ/G3S SoC
    it has been discovered that using the runtime PM API for suspend/resume
    (as will be proposed in the following commits) leads to the codec not
    being propertly initialized after resume. This is because w/o
    max_register populated to regmap_config the regcache_rbtree_sync()
    breaks on base_reg > max condition and the regcache_sync_block() call is
    skipped.
    
    Fixes: ef5c2eba2412 ("ASoC: codecs: Add da7213 codec")
    Cc: stable@vger.kernel.org
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Link: https://patch.msgid.link/20241106081826.1211088-23-claudiu.beznea.uj@bp.renesas.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/codecs/da7213.c
+++ sound/soc/codecs/da7213.c
@@ -1932,6 +1932,7 @@ static const struct regmap_config da7213_regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 8,
 
+	.max_register = DA7213_DAC_NG_CTRL,
 	.reg_defaults = da7213_reg_defaults,
 	.num_reg_defaults = ARRAY_SIZE(da7213_reg_defaults),
 	.volatile_reg = da7213_volatile_register,
