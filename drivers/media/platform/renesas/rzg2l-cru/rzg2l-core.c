cf0ca05b067a37e96ddecdba43b856e23a4754e9
Nghia Vo <nghia.vo.zn@renesas.com>
2025-01-22 16:55:56 +0700

    media: platform: rzg2l-cru: rzg2l-video: Move request_irq() to probe()
    Move request_irq() to probe(), in order to avoid requesting IRQ during
    device start which happens frequently. As this function is in probe(), it
    is better to replace it with its devm variant for managing the resource
    efficiently.
    
    While at it, drop the IRQF_SHARED flag as currently there is a single user
    for this IRQ.
    
    Suggested-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-core.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-core.c
@@ -273,7 +273,7 @@ static int rzg2l_cru_probe(struct platform_device *pdev)
 	struct rzg2l_cru_dev *cru;
 	struct device *dev = &pdev->dev;
 	struct v4l2_ctrl *ctrl;
-	int ret;
+	int irq, ret;
 
 	cru = devm_kzalloc(dev, sizeof(*cru), GFP_KERNEL);
 	if (!cru)
@@ -301,9 +301,14 @@ static int rzg2l_cru_probe(struct platform_device *pdev)
 	cru->dev = dev;
 	cru->info = of_device_get_match_data(dev);
 
-	cru->image_conv_irq = platform_get_irq(pdev, 0);
-	if (cru->image_conv_irq < 0)
-		return cru->image_conv_irq;
+	irq = platform_get_irq(pdev, 0);
+	if (irq < 0)
+		return irq;
+
+	ret = devm_request_irq(&pdev->dev, irq, rzg2l_cru_irq, 0,
+			       KBUILD_MODNAME, cru);
+	if (ret)
+		return dev_err_probe(&pdev->dev, ret, "failed to request irq\n");
 
 	platform_set_drvdata(pdev, cru);
 
