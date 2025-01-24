0b248c2d4951bf1f18f639891cfe930c770def9d
Quynh Nguyen <quynh.nguyen.xb@renesas.com>
2025-01-24 16:00:30 +0700

    phy: renesas: rcar-gen3-usb2: Remove ATF handler for PWRRDY signal
    Move to use sysc driver to handle PWRRDY signal for RZ/G3S.
    
    Signed-off-by: Quynh Nguyen <quynh.nguyen.xb@renesas.com>

--- drivers/phy/renesas/phy-rcar-gen3-usb2.c
+++ drivers/phy/renesas/phy-rcar-gen3-usb2.c
@@ -630,32 +630,6 @@ out:
 	return ret;
 }
 
-static int rz_g3s_phy_usb2_power_on(struct phy *p)
-{
-#if (defined(CONFIG_ARM) || defined(CONFIG_ARM64))
-	struct arm_smccc_res local_res;
-
-	/* Turning on the USB region power */
-	arm_smccc_smc(RZG3S_SIP_SVC_SET_USB_PWRRDY, RZG3S_SYS_USB_PWRRDY,
-		      RZG3S_SYS_USB_PWRRDY_PWRRDY, 0, 0, 0, 0, 0, &local_res);
-#endif
-
-	return rcar_gen3_phy_usb2_power_on(p);
-}
-
-static int rz_g3s_phy_usb2_power_off(struct phy *p)
-{
-#if (defined(CONFIG_ARM) || defined(CONFIG_ARM64))
-	struct arm_smccc_res local_res;
-
-	/* Turning off the USB region power */
-	arm_smccc_smc(RZG3S_SIP_SVC_SET_USB_PWRRDY, RZG3S_SYS_USB_PWRRDY,
-		      RZG3S_SYS_USB_PWRRDY_PWRRDY_N, 0, 0, 0, 0, 0, &local_res);
-#endif
-
-	return rcar_gen3_phy_usb2_power_off(p);
-}
-
 static const struct phy_ops rcar_gen3_phy_usb2_ops = {
 	.init		= rcar_gen3_phy_usb2_init,
 	.exit		= rcar_gen3_phy_usb2_exit,
@@ -667,14 +641,6 @@ static const struct phy_ops rcar_gen3_phy_usb2_ops = {
 static const struct phy_ops rz_g1c_phy_usb2_ops = {
 	.init		= rcar_gen3_phy_usb2_init,
 	.exit		= rcar_gen3_phy_usb2_exit,
-	.owner		= THIS_MODULE,
-};
-
-static const struct phy_ops rz_g3s_phy_usb2_ops = {
-	.init		= rcar_gen3_phy_usb2_init,
-	.exit		= rcar_gen3_phy_usb2_exit,
-	.power_on	= rz_g3s_phy_usb2_power_on,
-	.power_off	= rz_g3s_phy_usb2_power_off,
 	.owner		= THIS_MODULE,
 };
 
@@ -697,7 +663,7 @@ static const struct rcar_gen3_phy_drv_data rz_g2l_phy_usb2_data = {
 };
 
 static const struct rcar_gen3_phy_drv_data rz_g3s_phy_usb2_data = {
-	.phy_usb2_ops = &rz_g3s_phy_usb2_ops,
+	.phy_usb2_ops = &rcar_gen3_phy_usb2_ops,
 	.no_adp_ctrl = true,
 	.max_burst_length = 2,
 	.pwrrdy = true,
