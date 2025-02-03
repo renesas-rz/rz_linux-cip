2c8b636bb5783b7fcf88fc20fdd71dc0c2f9751d
Nghia Vo <nghia.vo.zn@renesas.com>
2025-02-03 16:24:12 +0700

    media: rzg2l-cru: Simplify handling of supported formats
    Refactor the handling of supported formats in the RZ/G2L CRU driver by
    adding `pixelformat` and `bpp` members to the `rzg2l_cru_ip_format`
    structure.
    
    New helper functions, `rzg2l_cru_ip_format_to_fmt()` and
    `rzg2l_cru_ip_index_to_fmt()`, are added to retrieve format information
    based on 4CC format and index, respectively. These helpers allow the
    removal of the now redundant `rzg2l_cru_format_from_pixel()` function.
    
    The new helpers are used in `rzg2l_cru_format_bytesperline()`,
    `rzg2l_cru_format_align()`, and `rzg2l_cru_enum_fmt_vid_cap()`,
    streamlining the handling of supported formats and improving code
    maintainability.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Link: https://lore.kernel.org/r/20241018133446.223516-14-prabhakar.mahadev-lad.rj@bp.renesas.com
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-ip.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-ip.c
@@ -12,10 +12,14 @@ static const struct rzg2l_cru_ip_format rzg2l_cru_ip_formats[] = {
 	{
 		.code = MEDIA_BUS_FMT_UYVY8_2X8,
 		.datatype = ICnMC_INF_YUV8_422,
+		.format = V4L2_PIX_FMT_UYVY,
+		.bpp = 2,
 	},
 	{
 		.code = MEDIA_BUS_FMT_UYVY8_1X16,
 		.datatype = ICnMC_INF_YUV8_422,
+		.format = V4L2_PIX_FMT_UYVY,
+		.bpp = 2,
 	},
 };
 
@@ -38,6 +42,26 @@ const struct rzg2l_cru_ip_format *rzg2l_cru_ip_code_to_fmt(unsigned int code)
 			return &rzg2l_cru_ip_formats[i];
 
 	return NULL;
+}
+
+const struct rzg2l_cru_ip_format *rzg2l_cru_ip_format_to_fmt(u32 format)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(rzg2l_cru_ip_formats); i++) {
+		if (rzg2l_cru_ip_formats[i].format == format)
+			return &rzg2l_cru_ip_formats[i];
+	}
+
+	return NULL;
+}
+
+const struct rzg2l_cru_ip_format *rzg2l_cru_ip_index_to_fmt(u32 index)
+{
+	if (index >= ARRAY_SIZE(rzg2l_cru_ip_formats))
+		return NULL;
+
+	return &rzg2l_cru_ip_formats[index];
 }
 
 struct v4l2_mbus_framefmt *rzg2l_cru_ip_get_src_fmt(struct rzg2l_cru_dev *cru)
