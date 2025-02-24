5ec78ef5985dd6e38962d4584e3243bc5699c390
Quang Nguyen <quang.nguyen.wx@renesas.com>
2025-02-24 14:07:18 +0700

    spi: spi-rspi-v2h: Convert to platform remove callback returning void
    The .remove() callback for a platform driver returns an int which makes
    many driver authors wrongly assume it's possible to do error handling by
    returning an error code. However the value returned is (mostly) ignored
    and this typically results in resource leaks. To improve here there is a
    quest to make the remove callback return void. In the first step of this
    quest all drivers are converted to .remove_new() which already returns
    void.
    
    Trivially convert this driver from always returning zero in the remove
    callback to the void returning variant.
    
    Referred to commit 72ec0e8f89a1 ("spi: rspi: Convert to platform remove callback returning void").
    
    Signed-off-by: Quang Nguyen <quang.nguyen.wx@renesas.com>

--- drivers/spi/spi-rspi-v2h.c
+++ drivers/spi/spi-rspi-v2h.c
@@ -830,14 +830,12 @@ static void rspi_release_dma(struct spi_controller *ctlr)
 		dma_release_channel(ctlr->dma_rx);
 }
 
-static int rspi_remove(struct platform_device *pdev)
+static void rspi_remove(struct platform_device *pdev)
 {
 	struct rspi_data *rspi = platform_get_drvdata(pdev);
 
 	rspi_release_dma(rspi->ctlr);
 	pm_runtime_disable(&pdev->dev);
-
-	return 0;
 }
 
 static const struct spi_ops rspi_v2h_ops = {
