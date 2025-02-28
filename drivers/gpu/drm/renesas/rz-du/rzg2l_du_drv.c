2e3eb23387f3d42f37631d573de6cd3152a23fe0
Nghia Vo <nghia.vo.zn@renesas.com>
2025-02-28 12:01:43 +0700

    drm: rz-du: Fix max dot clock for DPI
    As per the RZ/G2UL hardware manual Table 33.4 Clock List, the maximum
    dot clock for the DPI interface is 83.5 MHz. Add mode_valid callback
    to reject modes greater than 83.5 MHz.
    
    Also, add dotclock ranges for Parallel IF to get all the possible drm
    modes support.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
@@ -34,6 +34,8 @@ static const struct rzg2l_du_device_info rzg2l_du_r9a07g043u_info = {
 			.port = 0,
 		},
 	},
+	.min_dclk = 5802,
+	.max_dclk = 83500,
 };
 
 static const struct rzg2l_du_device_info rzg2l_du_r9a07g044_info = {
@@ -46,8 +48,10 @@ static const struct rzg2l_du_device_info rzg2l_du_r9a07g044_info = {
 		[RZG2L_DU_OUTPUT_DPAD0] = {
 			.possible_outputs = BIT(0),
 			.port = 1,
-		}
-	}
+		},
+	},
+	.min_dclk = 5802,
+	.max_dclk = 83500,
 };
 
 static const struct of_device_id rzg2l_du_of_table[] = {
