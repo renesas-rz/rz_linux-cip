1b74b5daa08970997cd4d2856dc5a39c3651d646
Nhat Nguyen <nhat.nguyen.xb@renesas.com>
2025-03-24 14:00:55 +0700

    pci: pcie-rzt2-window: fix outbound setting in endpoint mode
    Fix outbound setting in endpoint mode. cpu_addr value should be
    gotten directly from devicetree.
    
    Signed-off-by: Nhat Nguyen <nhat.nguyen.xb@renesas.com>
    Signed-off-by: Duan Le <duan.le.xa@renesas.com>

--- drivers/pci/controller/pcie-rzt2-window.c
+++ drivers/pci/controller/pcie-rzt2-window.c
@@ -104,8 +104,6 @@ void rzt2_pcie_ep_set_outbound(struct rzt2_pcie *pcie, int win, struct resource_
 	else
 		mask = 0x0;
 
-	cpu_addr = (cpu_addr - 0xA0000000) | 0x400000000UL;
-
 	/* PW0 addr: PCIE_WINDOW_BASEU_REG: 0x1104   PCIE_WINDOW_BASEL_REG: 0x1100 */
 	rzt2_pci_write_reg(pcie, (u32)(cpu_addr >> 32), PCIE_WINDOW_BASEU_REG(win));
 	rzt2_rmw(pcie, PCIE_WINDOW_BASEL_REG(win), 0xFFFFF000, (u32)(cpu_addr & 0xFFFFF000));
