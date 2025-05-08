583573326bd0a7c372dd0b5bd5c672350e41ae19
Nghia Vo <nghia.vo.zn@renesas.com>
2025-05-08 14:02:52 +0700

    sound: soc: rcar: Add set get clk rstc for S2R
    Get and set missing clk and rstc for S2R
    
    Signed-off-by: Nghia Vo <nghia.vo.zn@renesas.com>

--- sound/soc/sh/rcar/src.c
+++ sound/soc/sh/rcar/src.c
@@ -497,6 +497,8 @@ static int rsnd_src_quit(struct rsnd_mod *mod,
 			 struct rsnd_priv *priv)
 {
 	struct rsnd_src *src = rsnd_mod_to_src(mod);
+	struct device *dev = rsnd_priv_to_dev(priv);
+	struct clk *clk;
 
 	rsnd_src_halt(mod);
 
@@ -504,6 +506,11 @@ static int rsnd_src_quit(struct rsnd_mod *mod,
 
 	/* reset sync convert_rate */
 	src->sync.val = 0;
+
+	clk = devm_clk_get_optional(dev, "scu_supply_clk");
+	if (IS_ERR(clk))
+		dev_dbg(dev, "Not use scu_supply_clk\n");
+	clk_disable_unprepare(clk);
 
 	return 0;
 }
@@ -686,6 +693,7 @@ int rsnd_src_probe(struct rsnd_priv *priv)
 	clk = devm_clk_get_optional(dev, "scu_clk");
 	if (IS_ERR(clk))
 		dev_dbg(dev, "Not use scu_clk\n");
+	priv->clk_scu = clk;
 
 	ret = clk_prepare_enable(clk);
 	if (ret < 0)
@@ -695,6 +703,7 @@ int rsnd_src_probe(struct rsnd_priv *priv)
 	if (IS_ERR(clk))
 		dev_dbg(dev, "Not use scu_clkx2\n");
 
+	priv->clk_scux2 = clk;
 	ret = clk_prepare_enable(clk);
 	if (ret < 0)
 		dev_dbg(dev, "Can not enable scu_clkx2\n");
