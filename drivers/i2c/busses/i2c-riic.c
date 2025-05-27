d2cef03071b0b9191b6ae99d7f61502cc97b7f98
Hiep Pham <hiep.pham.zy@renesas.com>
2025-05-27 23:36:53 +0700

    2c: i2c-riic: update clock setting for lower speed transfer
    RIIC can support lower 100kHz clock frequency but clock setting is not
    working correctly.
    So, correct it by:
    - Set the maximum of CKS to 7 (div 128) based on HW manual.
    - Set SCLE and NFE bits to IICFER register to ensure they are set.
    - Update the duty cycle to 50% for lower speed (less than 50kHz).
    Also add a step to check bus busy state after stop condition occurs
    to ensure bus is released for the next transfer.
    
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>
    Signed-off-by: Tu Nguyen <tu.nguyen.xg@renesas.com>
    Signed-off-by: Hiep Pham <hiep.pham.zy@renesas.com>

--- drivers/i2c/busses/i2c-riic.c
+++ drivers/i2c/busses/i2c-riic.c
@@ -69,6 +69,8 @@
 #define ICMR3_ACKBT	BIT(3)
 
 #define ICFER_FMPE	BIT(7)
+#define	ICFER_SCLE	BIT(6)
+#define	ICFER_NFE	BIT(5)
 
 #define ICIER_TIE	BIT(7)
 #define ICIER_TEIE	BIT(6)
@@ -166,7 +168,7 @@ static int riic_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 	struct device *dev = adap->dev.parent;
 	unsigned long time_left;
 	int i, ret;
-	u8 start_bit;
+	u8 start_bit, val;
 
 	ret = pm_runtime_resume_and_get(dev);
 	if (ret)
@@ -198,6 +200,15 @@ static int riic_xfer(struct i2c_adapter *adap, struct i2c_msg msgs[], int num)
 			break;
 
 		start_bit = ICCR2_RS;
+	}
+
+	/* Should check bus state after finishing transfer */
+	if (!riic->err) {
+		time_left = readb_relaxed_poll_timeout(riic->base + riic->info->regs[RIIC_ICCR2],
+							val, !(val & ICCR2_BBSY), 10, 100);
+		if (time_left)
+			dev_warn(riic->adapter.dev.parent,
+				 "The i2c bus is still busy\n");
 	}
 
  out:
@@ -351,6 +362,9 @@ static int riic_init_hw(struct riic_dev *riic)
 
 	rate = clk_get_rate(riic->clk);
 
+	riic_clear_set_bit(riic, 0, ICFER_SCLE | ICFER_NFE,
+				riic->info->regs[RIIC_ICFER]);
+
 	/*
 	 * Assume the default register settings:
 	 *  FER.SCLE = 1 (SCL sync circuit enabled, adds 2 or 3 cycles)
@@ -364,16 +378,18 @@ static int riic_init_hw(struct riic_dev *riic)
 	/*
 	 * Determine reference clock rate. We must be able to get the desired
 	 * frequency with only 62 clock ticks max (31 high, 31 low).
-	 * Aim for a duty of 60% LOW, 40% HIGH.
+	 * Aim for a duty of:
+	 * - Below 50kHz: 50% LOW, 50% HIGH.
+	 * - Above 50kHz: 60% LOW, 40% HIGH.
 	 */
 	total_ticks = DIV_ROUND_UP(rate, t->bus_freq_hz ?: 1);
 
-	for (cks = 0; cks < 7; cks++) {
+	for (cks = 0; cks < 8; cks++) {
 		/*
-		 * 60% low time must be less than BRL + 2 + 1
+		 * Period of low time (60% or 50%) must be less than BRL + 2 + 1
 		 * BRL max register value is 0x1F.
 		 */
-		brl = ((total_ticks * 6) / 10);
+		brl = ((total_ticks * ((t->bus_freq_hz >= 50000) ? 6 : 5)) / 10);
 		if (brl <= (0x1F + 3))
 			break;
 
