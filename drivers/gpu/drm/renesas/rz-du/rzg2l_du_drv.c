af7836ae119685cc4ba0bd8fc5ad7d200760a198
Nguyen Khang Huy <huy.nguyen-khang@banvien.com.vn>
2025-05-27 16:28:57 +0700

    drm: renesas: rz-du: add Display Unit for RZ/N2H
    Add Display Unit for RZ/N2H.
    RZ/N2H has only one parallel output as same as RZ/G2UL
    but has different clock setting and do not have reset control.
    
    Signed-off-by: Nguyen Khang Huy <huy.nguyen-khang@banvien.com.vn>
    Signed-off-by: Quan Vu <quan.vu.ur@bp.renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
@@ -53,6 +53,7 @@ static const struct of_device_id rzg2l_du_of_table[] = {
 	{ .compatible = "renesas,r9a07g043u-du", .data = &rzg2l_du_r9a07g043u_info },
 	{ .compatible = "renesas,r9a07g044-du", .data = &rzg2l_du_r9a07g044_info },
 	{ .compatible = "renesas,r9a09g077-du", .data = &rzg2l_du_r9a07g043u_info },
+	{ .compatible = "renesas,r9a09g087-du", .data = &rzg2l_du_r9a07g043u_info },
 	{ /* sentinel */ }
 };
 
