fb8c85971dce6242a4788e31976b5056c98480b5
Huy Nguyen <huy.nguyen.wh@renesas.com>
2025-05-28 14:24:52 +0700

    drivers: net: dsa: Initialize ETHSW timer
    This patch initialize ETHSW timer.
    
    Signed-off-by: LongLuu <long.luu.ur@renesas.com>
    Signed-off-by: Huy Nguyen <huy.nguyen.wh@renesas.com>

--- drivers/net/dsa/rzt2h_ethsw.c
+++ drivers/net/dsa/rzt2h_ethsw.c
@@ -15,6 +15,7 @@
 #include <linux/reset.h>
 #include <net/dsa.h>
 #include <linux/net/renesas/rzt2h-ethss.h>
+#include <linux/net/renesas/rzt2h_timer_hwtstamp.h>
 
 #include "rzt2h_ethsw.h"
 
@@ -934,6 +935,7 @@ static int ethsw_setup(struct dsa_switch *ds)
 	int port, vlan, ret;
 	struct dsa_port *dp;
 	u32 reg;
+	struct timespec64 now;
 
 	/* Validate that there is only 1 CPU port with index ETHSW_CPU_PORT */
 	dsa_switch_for_each_cpu_port(dp, ds) {
@@ -1005,6 +1007,11 @@ static int ethsw_setup(struct dsa_switch *ds)
 
 		ethsw_vlan_setup(ethsw, port);
 	}
+
+	/* Initialize both ethsw timer with current system time */
+	ktime_get_real_ts64(&now);
+	ethsw_time_init(ethsw->base, 0);
+	ethsw_time_init(ethsw->base, 1);
 
 	return 0;
 }
