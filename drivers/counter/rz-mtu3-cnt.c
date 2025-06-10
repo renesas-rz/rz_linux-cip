7c0bdc221283e1e54c08586b04014a5f2bf339eb
hienhuynh <hien.huynh.px@renesas.com>
2025-06-10 11:12:14 +0700

    counter: rz-mtu3-cnt: Fix warning when CONFIG_PM_SLEEP is not defined
    If CONFIG_PM_SLEEP is not defined, below warning occurs:
    
    |drivers/counter/rz-mtu3-cnt.c:854:12: warning: 'rz_mtu3_cnt_pm_runtime_resume' defined but not used [-Wunused-function]
    | static int rz_mtu3_cnt_pm_runtime_resume(struct device *dev)
    |            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    |drivers/counter/rz-mtu3-cnt.c:817:12: warning: 'rz_mtu3_cnt_pm_runtime_suspend' defined but not used [-Wunused-function]
    | static int rz_mtu3_cnt_pm_runtime_suspend(struct device *dev)
                ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    So, guard them with CONFIG_PM_SLEEP condition checking to avoid
    the warning.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

--- drivers/counter/rz-mtu3-cnt.c
+++ drivers/counter/rz-mtu3-cnt.c
@@ -818,6 +818,7 @@ static struct counter_comp rz_mtu3_device_ext[] = {
 				 rz_mtu3_ext_input_phase_clock_select_enum),
 };
 
+#ifdef CONFIG_PM_SLEEP
 static int rz_mtu3_cnt_pm_runtime_suspend(struct device *dev)
 {
 	struct rz_mtu3_cnt *const priv = dev_get_drvdata(dev);
@@ -931,6 +932,7 @@ static int rz_mtu3_cnt_pm_runtime_resume(struct device *dev)
 
 	return 0;
 }
+#endif
 
 static DEFINE_RUNTIME_DEV_PM_OPS(rz_mtu3_cnt_pm_ops,
 				 rz_mtu3_cnt_pm_runtime_suspend,
