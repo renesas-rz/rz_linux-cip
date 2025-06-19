b0f4a181c3a3195b617a1fbe941973bfe56f9dbe
Duan Le <duan.le.xa@renesas.com>
2025-06-19 08:24:20 +0700

    drivers: net: ethernet: renesas: move ethss struct to rzt2-ethss header file.
    Move ethss struct to header file so that all module related
    to ETHSS can refer.
    
    Signed-off-by: Duan Le <duan.le.xa@renesas.com>

--- drivers/net/ethernet/renesas/rzt2_ethss.c
+++ drivers/net/ethernet/renesas/rzt2_ethss.c
@@ -125,21 +125,6 @@ static const char *index_to_string[ETHSS_MODCTRL_CONF_CONV_NUM] = {
 };
 
 /**
- * struct ethss - MII converter structure
- * @base: base address of the MII converter
- * @dev: Device associated to the MII converter
- * @lock: Lock used for read-modify-write access
- */
-struct ethss {
-	void __iomem *base;
-	struct device *dev;
-	struct clk *clk;
-	struct reset_control *rst_ethss;
-	struct reset_control *rst_conv;
-	spinlock_t lock;
-};
-
-/**
  * struct ethss_port - Per port MII converter struct
  * @ethss: backiling to MII converter structure
  * @port: port number
--- include/linux/net/renesas/rzt2-ethss.h
+++ include/linux/net/renesas/rzt2-ethss.h
@@ -3,6 +3,21 @@
 #ifndef __RZT2_ETHSS_H__
 #define __RZT2_ETHSS_H__
 
+/**
+ * struct ethss - MII converter structure
+ * @base: base address of the MII converter
+ * @dev: Device associated to the MII converter
+ * @lock: Lock used for read-modify-write access
+ */
+struct ethss {
+	void __iomem *base;
+	struct device *dev;
+	struct clk *clk;
+	struct reset_control *rst_ethss;
+	struct reset_control *rst_conv;
+	spinlock_t lock;
+};
+
 struct phylink;
 struct device_node;
 
