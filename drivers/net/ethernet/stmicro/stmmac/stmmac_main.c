8e4d13a156bf28550238c0b168b4d39f24599c7a
Huy Nguyen <huy.nguyen.wh@renesas.com>
2025-07-01 09:33:53 +0700

    net: stmmac: address non-LPI resume failures properly
    The Synopsys Designware GMAC core databook requires all clocks to be
    active in order to complete software reset, which we perform during
    resume.
    
    However, IEEE 802.3 allows a PHY to stop its clocks when placed in
    low-power mode, which happens when the system is suspended and WoL
    is not enabled.
    
    As an attempt to work around this, commit 36d18b5 ("net: stmmac:
    start phylink instance before stmmac_hw_setup()") started phylink
    early, but this has the side effect that the mac_link_up() method may
    be called before or during the initialisation of GMAC hardware.
    
    We also have the socfpga glue driver directly calling phy_resume()
    also as an attempt to work around this.
    
    In a previous commit, phylink_prepare_resume() has been introduced
    to give MAC drivers a way to ensure that the PHY is resumed prior to
    their initialisation of their MAC hardware. This commit adds the call,
    and moves the phylink_resume() call back to where it should be before
    the aforementioned commit.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://patch.msgid.link/E1tvO6a-008Vjh-FG@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>x
    Signed-off-by: Huy Nguyen <huy.nguyen.wh@renesas.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -8061,16 +8061,11 @@ int stmmac_resume(struct device *dev)
 	}
 
 	rtnl_lock();
-	if (device_may_wakeup(priv->device) && priv->plat->pmt) {
-		phylink_resume(priv->phylink);
-	} else {
-		phylink_resume(priv->phylink);
-		if (device_may_wakeup(priv->device))
-			phylink_speed_up(priv->phylink);
-	}
-	rtnl_unlock();
+	/* Prepare the PHY to resume, ensuring that its clocks which are
+	 * necessary for the MAC DMA reset to complete are running
+	 */
+	phylink_prepare_resume(priv->phylink);
 
-	rtnl_lock();
 	mutex_lock(&priv->lock);
 
 	stmmac_reset_queues_param(priv);
@@ -8088,6 +8083,15 @@ int stmmac_resume(struct device *dev)
 	stmmac_enable_all_dma_irq(priv);
 
 	mutex_unlock(&priv->lock);
+
+	/* phylink_resume() must be called after the hardware has been
+	 * initialised because it may bring the link up immediately in a
+	 * workqueue thread, which will race with initialisation.
+	 */
+	phylink_resume(priv->phylink);
+	if (device_may_wakeup(priv->device) && !priv->plat->pmt)
+		phylink_speed_up(priv->phylink);
+
 	rtnl_unlock();
 
 	netif_device_attach(ndev);
