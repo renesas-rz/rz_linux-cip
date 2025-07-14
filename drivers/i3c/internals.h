1dc27035a32610bb6713eb02c4bfdea4cee97835
Long Nguyen <long.nguyen.xd@bp.renesas.com>
2025-07-14 11:30:01 +0700

    i3c: master: export i3c_masterdev_type
    Export i3c_masterdev_type symbol so i3cdev module can verify if an
    i3c device is a master device.
    
    Signed-off-by: Vitor Soares <vitor.soares@synopsys.com>
    Signed-off-by: Duy Dang <duy.dang.xt@bp.renesas.com>

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -11,6 +11,8 @@
 #include <linux/i3c/master.h>
 #include <linux/io.h>
 
+extern const struct device_type i3c_masterdev_type;
+
 void i3c_bus_normaluse_lock(struct i3c_bus *bus);
 void i3c_bus_normaluse_unlock(struct i3c_bus *bus);
 
