94fd9aa6da1990814c49b52e04b39512a41c163c
Y Pham <y.pham.yg@bp.renesas.com>
2025-07-17 11:24:25 +0700

    pwm: rzg2l-gpt: Support input capture
    Support the following mode:
    - Input capture: add gtcia and gtcib interrupt.
    - Add gpt_irq_desc struct to optimize irq implementation.
    
    Signed-off-by: Y Pham <y.pham.yg@bp.renesas.com>
    Signed-off-by: Sang Nguyen <sang.nguyen-hoang@banvien.com.vn>

--- drivers/pwm/pwm-rzg2l-gpt.c
+++ drivers/pwm/pwm-rzg2l-gpt.c
@@ -35,11 +35,13 @@
 #include <linux/interrupt.h>
 #include <linux/regmap.h>
 #include <linux/mfd/syscon.h>
+#include <linux/wait.h>
 
 #define RZG2L_GET_CH(hwpwm)	((hwpwm) / 2)
 #define RZG2L_GET_HWPWM(ch, sub_ch) ((ch) * 2 + (sub_ch))
 #define RZG2L_GET_CH_OFFS(ch)	(0x100 * (ch))
 
+#define RZG2L_GTICxSR(ch, sub_ch)	(0x24 + RZG2L_GET_CH_OFFS(ch) + 4 * (sub_ch))
 #define RZG2L_GTCR(ch)		(0x2c + RZG2L_GET_CH_OFFS(ch))
 #define RZG2L_GTUDDTYC(ch)	(0x30 + RZG2L_GET_CH_OFFS(ch))
 #define RZG2L_GTIOR(ch)		(0x34 + RZG2L_GET_CH_OFFS(ch))
@@ -67,16 +69,23 @@
 #define RZG2L_GTCCRx_DOUBLE_BUFFER(sub_ch) \
 	((sub_ch) ? RZG2L_GTCCRB_DOUBLE_BUFFER : RZG2L_GTCCRA_DOUBLE_BUFFER)
 
+#define RZG2L_GTST_TCFA		BIT(0)
+#define RZG2L_GTST_TCFB		BIT(1)
 #define RZG2L_GTST_TCFPO	BIT(6)
 
 #define RZG2L_GTINTAD_GTINTPR_MASK	GENMASK(7, 6)
 #define RZG2L_GTINTPROV		BIT(6)
+
+#define RZG2L_GTINTAD_GTINTA	BIT(0)
+#define RZG2L_GTINTAD_GTINTB	BIT(1)
+#define RZG2L_GTINTAD_GTINTx(sub_ch)	((sub_ch) ? RZG2L_GTINTAD_GTINTB : RZG2L_GTINTAD_GTINTA)
 
 #define RZG2L_GTCR_CST		BIT(0)
 #define RZG2L_GTCR_MD		GENMASK(18, 16)
 #define RZG2L_GTCR_TPCS		GENMASK(26, 24)
 
 #define RZG2L_GTCR_MD_SAW_WAVE_PWM_MODE	FIELD_PREP(RZG2L_GTCR_MD, 0)
+#define RZG2L_GTCR_TPCS_P0_1024		0x05
 
 #define RZG2L_GTUDDTYC_UP	BIT(0)
 #define RZG2L_GTUDDTYC_UDF	BIT(1)
@@ -88,6 +97,13 @@
 #define RZG2L_GTIOR_OAE		BIT(8)
 #define RZG2L_GTIOR_OBE		BIT(24)
 #define RZG2L_GTIOR_OxE(sub_ch)		((sub_ch) ? RZG2L_GTIOR_OBE : RZG2L_GTIOR_OAE)
+
+#define RZG2L_GTIOR_NFAEN	BIT(13)
+#define RZG2L_GTIOR_NFBEN	BIT(29)
+#define RZG2L_GTIOR_NFCSA	GENMASK(15, 14)
+#define RZG2L_GTIOR_NFCSB	GENMASK(31, 30)
+#define RZG2L_GTIOR_NFCSx(sub_ch)	((sub_ch) ? RZG2L_GTIOR_NFCSB : RZG2L_GTIOR_NFCSA)
+#define RZG2L_GTIOR_NFCSx_P0_64		0x3
 
 #define RZG2L_INIT_OUT_HI_OUT_HI_END_TOGGLE	0x1b
 #define RZG2L_GTIOR_GTIOA_OUT_HI_END_TOGGLE_CMP_MATCH \
@@ -104,6 +120,20 @@
 #define RZG2L_MAX_PWM_CHANNELS	(RZG2L_MAX_HW_CHANNELS * RZG2L_CHANNELS_PER_IO)
 #define RZG2L_MAX_SCALE_FACTOR	1024
 #define RZG2L_MAX_TICKS		((u64)U32_MAX * RZG2L_MAX_SCALE_FACTOR)
+
+#define RZG2L_GTPR_MAX_VALUE			0xFFFFFFFF
+#define RZG2L_INPUT_CAP_GTIOB_BOTH_EDGE		0x0000F000
+#define RZG2L_INPUT_CAP_GTIOB_RISING_EDGE	0x00003000
+#define RZG2L_INPUT_CAP_GTIOB_FALLING_EDGE	0x0000C000
+#define RZG2L_INPUT_CAP_GTIOA_BOTH_EDGE		0x00000F00
+#define RZG2L_INPUT_CAP_GTIOA_RISING_EDGE	0x00000300
+#define RZG2L_INPUT_CAP_GTIOA_FALLING_EDGE	0x00000C00
+#define RZG2L_INPUT_CAP_GTIOx_BOTH_EDGE(sub_ch) \
+	((sub_ch) ? RZG2L_INPUT_CAP_GTIOB_BOTH_EDGE : RZG2L_INPUT_CAP_GTIOA_BOTH_EDGE)
+#define RZG2L_INPUT_CAP_GTIOx_RISING_EDGE(sub_ch) \
+	((sub_ch) ? RZG2L_INPUT_CAP_GTIOB_RISING_EDGE : RZG2L_INPUT_CAP_GTIOA_RISING_EDGE)
+#define RZG2L_INPUT_CAP_GTIOx_FALLING_EDGE(sub_ch) \
+	((sub_ch) ? RZG2L_INPUT_CAP_GTIOB_FALLING_EDGE : RZG2L_INPUT_CAP_GTIOA_FALLING_EDGE)
 
 /* Support GPT Error Interrupt Status Control for RZ/G3L only */
 #define RZG3L_PEISR_OFFSET		0x0088
@@ -142,6 +172,19 @@ enum {
 	NR_IRQ_TYPE,
 };
 
+struct gpt_irq_desc {
+	char *name;
+	irq_handler_t isr;
+	int res_num;
+};
+
+struct rz_gpt_cpt_data {
+	u64 snapshot[3];
+	unsigned int index;
+	unsigned int overflow_count;
+	wait_queue_head_t wait;
+};
+
 struct rz_gpt_pwm_channel {
 	unsigned int buffer_mode_count;
 	unsigned long buffer[NR_BUFFER];
@@ -154,6 +197,7 @@ struct rzg2l_gpt_chip {
 	struct regmap *icu_regmap;
 	struct mutex mutex; /* lock to protect shared channel resources */
 	const struct rz_gpt_data_cfg *cfg;
+	struct rz_gpt_cpt_data *cpt_data;
 	unsigned long rate_khz;
 	u32 period_ticks[RZG2L_MAX_HW_CHANNELS];
 	u8 channel_request[RZG2L_MAX_HW_CHANNELS];
@@ -488,6 +532,104 @@ static int rzg2l_gpt_apply(struct pwm_chip *chip, struct pwm_device *pwm,
 	return ret;
 }
 
+static int rzg2l_gpt_capture(struct pwm_chip *chip, struct pwm_device *pwm,
+			     struct pwm_capture *result, unsigned long timeout)
+{
+	struct rzg2l_gpt_chip *rzg2l_gpt = to_rzg2l_gpt_chip(chip);
+	struct rz_gpt_cpt_data *cpt_data = &rzg2l_gpt->cpt_data[pwm->hwpwm];
+	u8 ch = RZG2L_GET_CH(pwm->hwpwm);
+	u8 sub_ch = rzg2l_gpt_subchannel(pwm->hwpwm);
+	u64 high, low;
+	int ret;
+
+	if (rzg2l_gpt->channel_enable[ch]) {
+		dev_err(&rzg2l_gpt->chip->dev,
+			"Please keep pwm%d and pwm%d are not enabled to use input capture\n",
+			pwm->hwpwm, rzg2l_gpt_sibling(pwm->hwpwm));
+		return -EBUSY;
+	}
+
+	guard(mutex)(&rzg2l_gpt->mutex);
+
+	result->period = result->duty_cycle = 0;
+	cpt_data->index = 0;
+	cpt_data->snapshot[0] = 0;
+	cpt_data->snapshot[1] = 0;
+	cpt_data->snapshot[2] = 0;
+	cpt_data->overflow_count = 0;
+
+	/*
+	 * Prepare capture measurement:
+	 * - Set operating mode GTCR.MD[2:0] and count clock GTCR.TPCS[2:0]
+	 * - Using lowest frequency P0/1024 to avoid overflow
+	 */
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTCR(ch), RZG2L_GTCR_MD,
+			RZG2L_GTCR_MD_SAW_WAVE_PWM_MODE);
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTCR(ch), RZG2L_GTCR_TPCS,
+			FIELD_PREP(RZG2L_GTCR_TPCS, RZG2L_GTCR_TPCS_P0_1024));
+
+	/* Set count direction with GTUDDTYC[1:0]*/
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTUDDTYC(ch), RZG2L_GTUDDTYC_UP);
+
+	/* Set cycle in GTPR */
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTPR(ch), RZG2L_GTPR_MAX_VALUE);
+
+	/* Set initial value in GTCNT */
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTCNT(ch), 0);
+
+	/*
+	 * Set input pin as capture mode:
+	 * - Using noise filter with P0/64 clock
+	 */
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTIOR(ch), RZG2L_GTIOR_NFCSx(sub_ch),
+			RZG2L_GTIOR_NFCSx_P0_64);
+
+	/* Select input capture source in GTICASR and GTICBSR */
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, sub_ch),
+			RZG2L_INPUT_CAP_GTIOx_RISING_EDGE(sub_ch));
+
+	/* Enable input capture and overflow interrupt*/
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTINTAD(ch),
+			RZG2L_GTINTAD_GTINTx(sub_ch) | RZG2L_GTINTPROV);
+
+	/* Start count operation set GTCR.CST to 1 to start count operation*/
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTCR(ch), RZG2L_GTCR_CST, 1);
+
+	ret = wait_event_interruptible_timeout(cpt_data->wait, cpt_data->index > 1,
+		msecs_to_jiffies(timeout));
+
+	if (ret == -ERESTARTSYS)
+		goto out;
+
+	switch (cpt_data->index) {
+	case 0:
+	case 1:
+		result->period = 0;
+		result->duty_cycle = 0;
+		break;
+	case 2:
+		high = cpt_data->snapshot[1] - cpt_data->snapshot[0];
+		low = cpt_data->snapshot[2] - cpt_data->snapshot[1];
+		result->period = mul_u64_u64_div_u64(high + low, rzg2l_gpt->rate_khz, USEC_PER_SEC);
+		result->duty_cycle = mul_u64_u64_div_u64(high, rzg2l_gpt->rate_khz, USEC_PER_SEC);
+		break;
+	default:
+		dev_err(&chip->dev, "Internal error\n");
+		break;
+	}
+out:
+	/* Disable capture operation */
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, sub_ch), 0);
+
+	/* Disable interrupt */
+	rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTINTAD(ch), 0);
+
+	/* Stop count */
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTCR(ch), RZG2L_GTCR_CST, 0);
+
+	return 0;
+}
+
 static int rzg2l_gpt_get_ch_from_irq(struct rzg2l_gpt_chip *rzg2l_gpt,
 				     int irq, unsigned int irq_type)
 {
@@ -524,6 +666,12 @@ static irqreturn_t gpt_gtciv_interrupt(int irq, void *data)
 			RZG3L_PEVSTATn_BIT(ch), ~RZG3L_PEVSTATn_BIT(ch));
 	}
 
+	/* Counting overflow triggered to support input capture mode */
+	if (rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTICxSR(ch, 0)))
+		rzg2l_gpt->cpt_data[RZG2L_GET_HWPWM(ch, 0)].overflow_count++;
+	else if (rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTICxSR(ch, 1)))
+		rzg2l_gpt->cpt_data[RZG2L_GET_HWPWM(ch, 1)].overflow_count++;
+
 	for_each_set_bit(sub_ch,
 			(unsigned long *)&rzg2l_gpt->channel_enable[ch],
 			RZG2L_CHANNELS_PER_IO) {
@@ -557,6 +705,104 @@ static irqreturn_t gpt_gtciv_interrupt(int irq, void *data)
 
 	/* Disable overflow interrupt flags */
 	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTST(ch), RZG2L_GTST_TCFPO, 0);
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t gpt_gtcia_interrupt(int irq, void *data)
+{
+	struct rzg2l_gpt_chip *rzg2l_gpt = data;
+	struct rz_gpt_cpt_data *cpt_data;
+	unsigned int pwm_id;
+	uint32_t tmp = 0;
+	int ch;
+
+	ch = rzg2l_gpt_get_ch_from_irq(rzg2l_gpt, irq, GTCIA);
+	if (ch < 0)
+		return IRQ_NONE;
+
+	guard(spinlock_irqsave)(&rzg2l_gpt->lock);
+
+	pwm_id = RZG2L_GET_HWPWM(ch, 0);
+
+	cpt_data = &rzg2l_gpt->cpt_data[pwm_id];
+
+	cpt_data->snapshot[cpt_data->index] =
+		rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTCCRAB(ch, 0)) +
+		(cpt_data->overflow_count) * RZG2L_GTPR_MAX_VALUE;
+
+	switch (cpt_data->index) {
+	case 0:
+	case 1:
+		tmp = rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTICxSR(ch, 0));
+		if (tmp & RZG2L_INPUT_CAP_GTIOA_RISING_EDGE)
+			rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 0),
+					RZG2L_INPUT_CAP_GTIOA_FALLING_EDGE);
+		if (tmp & RZG2L_INPUT_CAP_GTIOA_FALLING_EDGE)
+			rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 0),
+					RZG2L_INPUT_CAP_GTIOA_RISING_EDGE);
+		cpt_data->index++;
+		break;
+	case 2:
+		/* Disable capture operation */
+		rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 0), 0);
+		wake_up(&cpt_data->wait);
+		break;
+	default:
+		return IRQ_NONE;
+	}
+
+	/* Disable interrupt flags */
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTST(ch), RZG2L_GTST_TCFA, 0);
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t gpt_gtcib_interrupt(int irq, void *data)
+{
+	struct rzg2l_gpt_chip *rzg2l_gpt = data;
+	struct rz_gpt_cpt_data *cpt_data;
+	unsigned int pwm_id;
+	uint32_t tmp = 0;
+	int ch;
+
+	ch = rzg2l_gpt_get_ch_from_irq(rzg2l_gpt, irq, GTCIB);
+	if (ch < 0)
+		return IRQ_NONE;
+
+	guard(spinlock_irqsave)(&rzg2l_gpt->lock);
+
+	pwm_id = RZG2L_GET_HWPWM(ch, 1);
+
+	cpt_data = &rzg2l_gpt->cpt_data[pwm_id];
+
+	cpt_data->snapshot[cpt_data->index] =
+		rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTCCRAB(ch, 1)) +
+		(cpt_data->overflow_count) * RZG2L_GTPR_MAX_VALUE;
+
+	switch (cpt_data->index) {
+	case 0:
+	case 1:
+		tmp = rzg2l_gpt_read(rzg2l_gpt, RZG2L_GTICxSR(ch, 1));
+		if (tmp & RZG2L_INPUT_CAP_GTIOB_RISING_EDGE)
+			rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 1),
+					RZG2L_INPUT_CAP_GTIOB_FALLING_EDGE);
+		if (tmp & RZG2L_INPUT_CAP_GTIOB_FALLING_EDGE)
+			rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 1),
+					RZG2L_INPUT_CAP_GTIOB_RISING_EDGE);
+		cpt_data->index++;
+		break;
+	case 2:
+		/* Disable capture operation */
+		rzg2l_gpt_write(rzg2l_gpt, RZG2L_GTICxSR(ch, 1), 0);
+		wake_up(&cpt_data->wait);
+		break;
+	default:
+		return IRQ_NONE;
+	}
+
+	/* Disable interrupt flags */
+	rzg2l_gpt_modify(rzg2l_gpt, RZG2L_GTST(ch), RZG2L_GTST_TCFB, 0);
 
 	return IRQ_HANDLED;
 }
@@ -798,6 +1044,13 @@ static const struct pwm_ops rzg2l_gpt_ops = {
 	.free = rzg2l_gpt_free,
 	.get_state = rzg2l_gpt_get_state,
 	.apply = rzg2l_gpt_apply,
+	.capture = rzg2l_gpt_capture,
+};
+
+static const struct gpt_irq_desc gpt_irqs[] = {
+	{ .name = "gtcia_n", .isr = gpt_gtcia_interrupt, .res_num = GTCIA },
+	{ .name = "gtcib_n", .isr = gpt_gtcib_interrupt, .res_num = GTCIB },
+	{ .name = "gtciv_n", .isr = gpt_gtciv_interrupt, .res_num = GTCIV },
 };
 
 static int rzg2l_gpt_probe(struct platform_device *pdev)
@@ -840,17 +1093,20 @@ static int rzg2l_gpt_probe(struct platform_device *pdev)
 		return dev_err_probe(dev, PTR_ERR(rstc), "Cannot deassert reset control\n");
 
 	for (i = 0; i < RZG2L_MAX_HW_CHANNELS; i++) {
-		snprintf(irq_name, sizeof(irq_name), "gtciv_n_%d", i);
-		irq = platform_get_irq_byname(pdev, irq_name);
-		if (irq < 0)
-			return dev_err_probe(dev, irq, "Failed to obtain IRQ\n");
+		for (unsigned int j = 0; j < ARRAY_SIZE(gpt_irqs); j++) {
+			snprintf(irq_name, sizeof(irq_name), "%s_%d", gpt_irqs[j].name, i);
 
-		ret = devm_request_irq(dev, irq, gpt_gtciv_interrupt,
-				      0, dev_name(dev), rzg2l_gpt);
-		if (ret < 0)
-			return dev_err_probe(dev, ret, "Failed to request IRQ\n");
+			irq = platform_get_irq_byname(pdev, irq_name);
+			if (irq < 0)
+				return dev_err_probe(dev, irq, "Failed to obtain IRQ\n");
 
-		rzg2l_gpt->irq_map[i][GTCIV] = irq;
+			ret = devm_request_irq(dev, irq, gpt_irqs[j].isr,
+					       0, dev_name(dev), rzg2l_gpt);
+			if (ret < 0)
+				return dev_err_probe(dev, ret, "Failed to request IRQ\n");
+
+			rzg2l_gpt->irq_map[i][gpt_irqs[j].res_num] = irq;
+		}
 	}
 
 	clk = devm_clk_get_enabled(dev, NULL);
@@ -883,6 +1139,18 @@ static int rzg2l_gpt_probe(struct platform_device *pdev)
 
 	spin_lock_init(&rzg2l_gpt->lock);
 	mutex_init(&rzg2l_gpt->mutex);
+
+	rzg2l_gpt->cpt_data = devm_kcalloc(dev, RZG2L_MAX_PWM_CHANNELS,
+					   sizeof(*rzg2l_gpt->cpt_data),
+					   GFP_KERNEL);
+	if (!rzg2l_gpt->cpt_data)
+		return -ENOMEM;
+
+	for (i = 0; i < RZG2L_MAX_PWM_CHANNELS; i++) {
+		struct rz_gpt_cpt_data *cpt_data = &rzg2l_gpt->cpt_data[i];
+
+		init_waitqueue_head(&cpt_data->wait);
+	}
 
 	chip->ops = &rzg2l_gpt_ops;
 	rzg2l_gpt->chip = chip;
