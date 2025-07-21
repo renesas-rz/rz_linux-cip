60d4eec5c08ae98dfda94189e55a86cfad227011
Nguyen Anh Minh <minh.nguyen-anh@banvien.com.vn>
2025-07-21 15:01:31 +0700

    mmc: renesas_sdhi: Fix change point of data handling
    On latest kernel revisions it has been noticed (on a RZ/G3S system) that
    when booting Linux and root file system is on eMMC, at some point in
    the booting process, when the systemd applications are started, the
    "mmc0: tuning execution failed: -5" message is displayed on console.
    On kernel v6.7-rc5 this is reproducible in 90% of the boots. This was
    missing on the same system with kernel v6.5.0-rc1. It was also noticed on
    kernel revisions v6.6-rcX on a RZ/G2UL based system but not on the kernel
    this fix is based on (v6.7-rc5).
    
    Investigating it on RZ/G3S lead to the conclusion that every time the issue
    is reproduced all the probed TAPs are OK. According to datasheet, when this
    happens the change point of data need to be considered for tuning.
    
    Previous code considered the change point of data happens when the content
    of the SMPCMP register is zero. According to RZ/V2M hardware manual,
    chapter "Change Point of the Input Data" (as this is the most clear
    description that I've found about change point of the input data and all
    RZ hardware manual are similar on this chapter), at the time of tuning,
    data is captured by the previous and next TAPs and the result is stored in
    the SMPCMP register (previous TAP in bits 22..16, next TAP in bits 7..0).
    If there is a mismatch b/w the previous and the next TAPs, it indicates
    that there is a change point of the input data.
    
    To comply with this, the patch checks if this mismatch is present and
    updates the priv->smpcmp mask only if it is not. Previous code checked if
    the value of SMPCMP register was zero. However, on RZ/G3S, this leads to
    failues as it may happen, e.g., the following:
    CMPNGU=0x0e, CMPNGD=0x0e, SMPCMP=0x000e000e.
    
    Along with it, as mmc_send_tuning() may return with error even before the
    MMC command reach the controller (and because at that point cmd_error = 0),
    the update of priv->smpcmp mask has been done only if the return value of
    mmc_send_tuning(mmc, opcode, &cmd_error) is 0 (success).
    
    This change has been checked on the devices with the following DTSes by
    doing 100 consecutive boots and checking for the tuning failure message:
    - r9a08g045s33-smarc.dts
    - r8a7742-iwg21d-q7.dts
    - r8a7743-iwg20d-q7.dts
    - r8a7744-iwg20d-q7.dts
    - r8a7745-iwg22d-sodimm.dts
    - r8a77470-iwg23s-sbc.dts
    - r8a774a1-hihope-rzg2m-ex.dts
    - r8a774b1-hihope-rzg2n-ex.dts
    - r8a774c0-ek874.dts
    - r8a774e1-hihope-rzg2h-ex.dts
    - r9a07g043u11-smarc-rzg2ul.dts
    - r9a07g044c2-smarc-rzg2lc.dts
    - r9a07g044l2-smarc-rzg2l.dts
    - r9a07g054l2-smarc-rzv2l.dts
    
    Fixes: 5fb6bf51f6d1 ("mmc: renesas_sdhi: improve TAP selection if all TAPs are good")
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Signed-off-by: Nguyen Anh Minh <minh.nguyen-anh@banvien.com.vn>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -18,6 +18,7 @@
  *
  */
 
+#include <linux/bitfield.h>
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/iopoll.h>
@@ -314,6 +315,8 @@ static int renesas_sdhi_start_signal_voltage_switch(struct mmc_host *mmc,
 #define SH_MOBILE_SDHI_SCC_SMPCMP_CMD_REQDOWN	BIT(8)
 #define SH_MOBILE_SDHI_SCC_SMPCMP_CMD_REQUP	BIT(24)
 #define SH_MOBILE_SDHI_SCC_SMPCMP_CMD_ERR	(BIT(8) | BIT(24))
+#define SH_MOBILE_SDHI_SCC_SMPCMP_CMPNGU_DATA	GENMASK(23, 16)
+#define SH_MOBILE_SDHI_SCC_SMPCMP_CMPNGD_DATA	GENMASK(7, 0)
 
 #define SH_MOBILE_SDHI_SCC_TMPPORT2_HS400OSEL	BIT(4)
 #define SH_MOBILE_SDHI_SCC_TMPPORT2_HS400EN	BIT(31)
@@ -720,11 +723,18 @@ static int renesas_sdhi_execute_tuning(struct mmc_host *mmc, u32 opcode)
 		/* Set sampling clock position */
 		sd_scc_write32(host, priv, SH_MOBILE_SDHI_SCC_TAPSET, i % priv->tap_num);
 
-		if (mmc_send_tuning(mmc, opcode, &cmd_error) == 0)
-			set_bit(i, priv->taps);
+		if (mmc_send_tuning(mmc, opcode, &cmd_error) == 0) {
+			u32 val, cmpngu_data, cmpngd_data;
 
-		if (sd_scc_read32(host, priv, SH_MOBILE_SDHI_SCC_SMPCMP) == 0)
-			set_bit(i, priv->smpcmp);
+			val = sd_scc_read32(host, priv, SH_MOBILE_SDHI_SCC_SMPCMP);
+			cmpngu_data = FIELD_GET(SH_MOBILE_SDHI_SCC_SMPCMP_CMPNGU_DATA, val);
+			cmpngd_data = FIELD_GET(SH_MOBILE_SDHI_SCC_SMPCMP_CMPNGD_DATA, val);
+
+			if (cmpngu_data == cmpngd_data)
+				set_bit(i, priv->smpcmp);
+
+			set_bit(i, priv->taps);
+		}
 
 		if (cmd_error)
 			mmc_send_abort_tuning(mmc, opcode);
