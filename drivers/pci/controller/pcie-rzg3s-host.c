7b1b43dd42e5ac3f81be71146575f2d4aefb1848
Minh Nguyen <minh.nguyen.kx@renesas.com>
2025-08-08 10:38:48 +0700

    pci: pcie-rzg3s-host: Fix issue S2R failed
    When resuming from S2R, the SoC hangs because no clock is supplied
    for register access (similar to the board hang issue when no card is inserted).
    This commit disables the clock during suspend and enables it during resume
    to avoid this issue.
    
    Signed-off-by: Minh Nguyen <minh.nguyen.kx@renesas.com>

--- drivers/pci/controller/pcie-rzg3s-host.c
+++ drivers/pci/controller/pcie-rzg3s-host.c
@@ -1618,6 +1618,8 @@ static int rzg3s_pcie_suspend_noirq(struct device *dev)
 	struct regmap *sysc = host->sysc;
 	int ret;
 
+	clk_disable_unprepare(host->aclk);
+
 	ret = pm_runtime_put_sync(dev);
 	if (ret)
 		return ret;
@@ -1673,9 +1675,14 @@ static int rzg3s_pcie_resume_noirq(struct device *dev)
 	if (ret)
 		goto assert_power_resets;
 
+	ret = clk_prepare_enable(host->aclk);
+	if (ret) {
+		goto rpm_put;
+	}
+
 	ret = rzg3s_pcie_host_setup(host, NULL, rzg3s_pcie_msi_hw_setup, false);
 	if (ret)
-		goto rpm_put;
+		goto disable_clk;
 
 	return 0;
 
@@ -1683,6 +1690,8 @@ static int rzg3s_pcie_resume_noirq(struct device *dev)
 	 * If any error happens there is no way to recover the IP. Put it in the
 	 * lowest possible power state.
 	 */
+disable_clk:
+	clk_disable_unprepare(host->aclk);
 rpm_put:
 	pm_runtime_put_sync(dev);
 assert_power_resets:
