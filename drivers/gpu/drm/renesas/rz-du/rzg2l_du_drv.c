13b4b556049dfabc02984f728cb572f6e50b14de
hoahuynh <hoa.huynh.ud@renesas.com>
2025-08-29 15:59:00 +0700

    drm: renesas: rz-du: rzg2l_du_drv: Add RZ/T2H DU support
    Add RZ/T2H DU support
    
    Signed-off-by: hoahuynh <hoa.huynh.ud@renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_du_drv.c
@@ -52,6 +52,7 @@ static const struct rzg2l_du_device_info rzg2l_du_r9a07g044_info = {
 static const struct of_device_id rzg2l_du_of_table[] = {
 	{ .compatible = "renesas,r9a07g043u-du", .data = &rzg2l_du_r9a07g043u_info },
 	{ .compatible = "renesas,r9a07g044-du", .data = &rzg2l_du_r9a07g044_info },
+	{ .compatible = "renesas,r9a09g077-du", .data = &rzg2l_du_r9a07g043u_info },
 	{ /* sentinel */ }
 };
 
