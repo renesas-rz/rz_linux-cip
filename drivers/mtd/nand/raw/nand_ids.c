a8c264bb5147be4c9f3ec270188b07fdeeb2aef7
Anh Ly <anh.ly.yb@renesas.com>
2025-09-03 14:03:48 +0700

    mtd: rawnand: nand_ids: Add Micron MT29F32G08ABAAA flash entry
    Add entry for Micron MT29F32G08ABAAA (32Gbit, 3.3V, 8-bit bus) to
    nand_flash_ids[] with: 8KiB page, 1MiB erase block, 448B OOB,
    ECC 8 bits per 512B.
    
    The expected ID is {2c,68,00,27,a9} (or its 8-byte variant), but
    READID cannot be retrieved correctly on this platform, so the device
    is matched using the 2-byte ID {2c,68} and id_len=2.
    
    Signed-off-by: Anh Ly <anh.ly.yb@renesas.com>

--- drivers/mtd/nand/raw/nand_ids.c
+++ drivers/mtd/nand/raw/nand_ids.c
@@ -26,6 +26,9 @@ struct nand_flash_dev nand_flash_ids[] = {
 	 * listed by full ID. We list them first so that we can easily identify
 	 * the most specific match.
 	 */
+	{"MT29F32G08ABAAA 32G 3.3V 8-bit",
+		{ .id = {0x2c, 0x68} },
+		  SZ_8K, SZ_4K, SZ_1M, 0, 2, 448, NAND_ECC_INFO(8, SZ_512), },
 	{"TC58NVG0S3E 1G 3.3V 8-bit",
 		{ .id = {0x98, 0xd1, 0x90, 0x15, 0x76, 0x14, 0x01, 0x00} },
 		  SZ_2K, SZ_128, SZ_128K, 0, 8, 64, NAND_ECC_INFO(1, SZ_512), },
