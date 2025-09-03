f98b762049a984dce954a38c96ad672feab56238
Minh Nguyen <minh.nguyen.kx@renesas.com>
2025-09-03 10:46:16 +0700

    net: stmmac: provide set_clk_tx_rate() hook
    Several stmmac sub-drivers which support RGMII follow the same pattern.
    They calculate the transmit clock rate, and then call clk_set_rate().
    
    Analysis of several implementation documents suggests that the platform
    is responsible for providing the transmit clock to the DWMAC core's
    clk_tx_i. The expected rates are:
    
    	10Mbps	100Mbps	1Gbps
    MII	2.5MHz	25MHz
    RMII	2.5MHz	25MHz
    GMII			125MHz
    RGMI	2.5MHz	25MHz	125MHz
    
    It seems some platforms require this clock to be manually configured,
    but there are outputs from the MAC core that indicate the speed, so a
    platform may use these to automatically configure the clock. Thus, we
    can't just provide one solution to configure this clock rate.
    
    Moreover, the clock may need to be derived from one of several sources
    depending on the interface mode.
    
    Provide a platform hook that is passed the transmit clock, interface
    mode and speed.
    
    Reviewed-by: default avatarThierry Reding <treding@nvidia.com>
    Signed-off-by: default avatarRussell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Reviewed-by: default avatarAndrew Lunn <andrew@lunn.ch>
    Link: https://patch.msgid.link/E1tna0F-0052sS-Lr@rmk-PC.armlinux.org.uk
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Minh Nguyen <minh.nguyen.kx@renesas.com>

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -76,6 +76,7 @@
 			| DMA_AXI_BLEN_32 | DMA_AXI_BLEN_64 \
 			| DMA_AXI_BLEN_128 | DMA_AXI_BLEN_256)
 
+struct clk;
 struct stmmac_priv;
 
 /* Platfrom data for platform device structure's platform_data field */
@@ -229,6 +230,8 @@ struct plat_stmmacenet_data {
 	u8 tx_sched_algorithm;
 	struct stmmac_rxq_cfg rx_queues_cfg[MTL_MAX_RX_QUEUES];
 	struct stmmac_txq_cfg tx_queues_cfg[MTL_MAX_TX_QUEUES];
+	int (*set_clk_tx_rate)(void *priv, struct clk *clk_tx_i,
+			       phy_interface_t interface, int speed);
 	void (*fix_mac_speed)(void *priv, unsigned int speed, unsigned int mode);
 	int (*fix_soc_reset)(void *priv, void __iomem *ioaddr);
 	int (*serdes_powerup)(struct net_device *ndev, void *priv);
@@ -250,6 +253,7 @@ struct plat_stmmacenet_data {
 	struct clk *stmmac_clk;
 	struct clk *pclk;
 	struct clk *clk_ptp_ref;
+	struct clk *clk_tx_i;
 	unsigned int clk_ptp_rate;
 	unsigned int clk_ref_rate;
 	struct clk_bulk_data *clks;
