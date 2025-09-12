4c2cf69e189071bfb5369dd604666a6a5cf18df7
Ngo Phuong Nam <nam.ngo-phuong@banvien.com.vn>
2025-09-12 14:44:52 +0700

    watchdog: Add support RZ/N2H Watchdog
    Add Watchdog compatible string support for RZ/N2H.
    Watchdog of RZ/N2H is similar to RZ/T2H.
    
    Signed-off-by: Anh Ly <anh.ly.yb@renesas.com>
    Signed-off-by: Ngo Phuong Nam <nam.ngo-phuong@banvien.com.vn>

--- drivers/watchdog/rzt2h_wdt.c
+++ drivers/watchdog/rzt2h_wdt.c
@@ -260,6 +260,7 @@ static int rzt2h_wdt_probe(struct platform_device *pdev)
 
 static const struct of_device_id rzt2h_wdt_ids[] = {
 	{ .compatible = "renesas,r9a09g077-wdt", },
+	{ .compatible = "renesas,r9a09g087-wdt", },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, rzt2h_wdt_ids);
