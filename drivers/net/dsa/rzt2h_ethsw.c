293bda6b88dabe828cb2d2d91c11678892c511f8
Tran Thanh Toan <toan.tran-thanh@banvien.com.vn>
2025-09-16 11:44:50 +0700

    drivers: net: dsa: Add Renesas RZ/N2H support
    This commit support for RZ/N2H.
    
    Signed-off-by: Vo Dai Mau <mau.vo-dai@banvien.com.vn>
    Signed-off-by: Tran Thanh Toan <toan.tran-thanh@banvien.com.vn>

--- drivers/net/dsa/rzt2h_ethsw.c
+++ drivers/net/dsa/rzt2h_ethsw.c
@@ -2128,6 +2128,7 @@ static void ethsw_shutdown(struct platform_device *pdev)
 
 static const struct of_device_id ethsw_of_mtable[] = {
 	{ .compatible = "renesas,rzt2h-ethsw", },
+	{ .compatible = "renesas,rzn2h-ethsw", },
 	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, ethsw_of_mtable);
