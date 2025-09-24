b2d55abbf6effe1a0056a9d3852c2dbcfed5daa7
Quang Nguyen <quang.nguyen.wx@renesas.com>
2025-09-24 10:29:24 +0700

    can: rcar_canfd: Convert to DEFINE_SIMPLE_DEV_PM_OPS()
    Convert the Renesas R-Car CAN-FD driver from SIMPLE_DEV_PM_OPS() to
    DEFINE_SIMPLE_DEV_PM_OPS() and pm_sleep_ptr().  This lets us drop the
    __maybe_unused annotations from its suspend and resume callbacks, and
    reduces kernel size in case CONFIG_PM or CONFIG_PM_SLEEP is disabled.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Link: https://patch.msgid.link/20251124102837.106973-7-biju.das.jz@bp.renesas.com
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Quang Nguyen <quang.nguyen.wx@renesas.com>

--- drivers/net/can/rcar/rcar_canfd.c
+++ drivers/net/can/rcar/rcar_canfd.c
@@ -2292,7 +2292,7 @@ static void rcar_canfd_remove(struct platform_device *pdev)
 	pm_runtime_disable(gpriv->dev);
 }
 
-static int __maybe_unused rcar_canfd_suspend(struct device *dev)
+static int rcar_canfd_suspend(struct device *dev)
 {
 	struct rcar_canfd_global *gpriv = dev_get_drvdata(dev);
 	u32 ch;
@@ -2316,7 +2316,7 @@ static int __maybe_unused rcar_canfd_suspend(struct device *dev)
 	return 0;
 }
 
-static int __maybe_unused rcar_canfd_resume(struct device *dev)
+static int rcar_canfd_resume(struct device *dev)
 {
 	struct rcar_canfd_global *gpriv = dev_get_drvdata(dev);
 	int err;
@@ -2380,8 +2380,8 @@ fail_dev:
 	return err;
 }
 
-static SIMPLE_DEV_PM_OPS(rcar_canfd_pm_ops, rcar_canfd_suspend,
-			 rcar_canfd_resume);
+static DEFINE_SIMPLE_DEV_PM_OPS(rcar_canfd_pm_ops, rcar_canfd_suspend,
+				rcar_canfd_resume);
 
 static const __maybe_unused struct of_device_id rcar_canfd_of_table[] = {
 	{ .compatible = "renesas,r8a779a0-canfd", .data = &rcar_gen4_hw_info },
@@ -2399,7 +2399,7 @@ static struct platform_driver rcar_canfd_driver = {
 	.driver = {
 		.name = RCANFD_DRV_NAME,
 		.of_match_table = of_match_ptr(rcar_canfd_of_table),
-		.pm = &rcar_canfd_pm_ops,
+		.pm = pm_sleep_ptr(&rcar_canfd_pm_ops),
 	},
 	.probe = rcar_canfd_probe,
 	.remove = rcar_canfd_remove,
