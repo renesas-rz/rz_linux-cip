1183d00357605942ef4e8a976b3e36c2873fbb4e
Minh Nguyen <minh.nguyen.kx@renesas.com>
2025-09-30 14:09:00 +0700

    pci: pcie-rzg3s-host: Support PCIe RC for V2N
    This commit supports PCIe RC for RZ/V2N SoC.
    
    Signed-off-by: Minh Nguyen <minh.nguyen.kx@renesas.com>

--- drivers/pci/controller/pcie-rzg3s-host.c
+++ drivers/pci/controller/pcie-rzg3s-host.c
@@ -1994,6 +1994,10 @@ static const struct of_device_id rzg3s_pcie_of_match[] = {
 		.compatible = "renesas,r9a09g047-pcie",
 		.data = &rzv2h_soc_data,
 	},
+	{
+		.compatible = "renesas,r9a09g056-pcie",
+		.data = &rzv2h_soc_data,
+	},
 	{},
 };
 
