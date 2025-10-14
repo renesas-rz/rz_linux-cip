b51cfaf9af688db74a601875501eb45b5a3bf615
Le Huu Minh Duc <duc.le.xz@bp.renesas.com>
2025-10-14 13:23:46 +0700

    clocksource: rz-mtu3-clk: Support S2R for MTU3 clocksource
    Implement PM suspend/resume functions to support S2R for MTU3
    clocksource driver.
    
    Signed-off-by: Minh Nguyen <minh.nguyen.kx@renesas.com>
    Signed-off-by: Le Huu Minh Duc <duc.le.xz@bp.renesas.com>

--- drivers/clocksource/rz_mtu3_clk.c
+++ drivers/clocksource/rz_mtu3_clk.c
@@ -271,20 +271,27 @@ static void rz_mtu3_clk_clocksource_suspend(struct clocksource *cs)
 {
 	struct rz_mtu3_clk_channel_priv *ch = cs_to_sh_mtu(cs);
 
-	if (!ch->cs_enabled)
-		return;
-	rz_mtu3_clk_stop(ch, FLAG_CLOCKSOURCE);
+	if (ch->cs_enabled)
+		rz_mtu3_clk_stop(ch, FLAG_CLOCKSOURCE);
 	dev_pm_genpd_suspend(&ch->mtu->pdev->dev);
+	reset_control_assert(ch->mtu->rstc);
 }
 
 static void rz_mtu3_clk_clocksource_resume(struct clocksource *cs)
 {
 	struct rz_mtu3_clk_channel_priv *ch = cs_to_sh_mtu(cs);
+	int ret;
 
-	if (!ch->cs_enabled)
+	ret = reset_control_deassert(ch->mtu->rstc);
+	if (ret) {
+		dev_err(&ch->mtu->pdev->dev, "failed to deassert reset control\n");
+		reset_control_assert(ch->mtu->rstc);
 		return;
+	}
+
 	dev_pm_genpd_resume(&ch->mtu->pdev->dev);
-	rz_mtu3_clk_start(ch, FLAG_CLOCKSOURCE);
+	if (ch->cs_enabled)
+		rz_mtu3_clk_start(ch, FLAG_CLOCKSOURCE);
 }
 
 static void rz_mtu3_clk_register_clockevent(struct rz_mtu3_clk_channel_priv *ch,
@@ -406,6 +413,17 @@ static int rz_mtu3_clk_setup(struct rz_mtu3_clk_device *mtu,
 	mtu->pdev = pdev;
 	raw_spin_lock_init(&mtu->lock);
 
+	/* Get hold of reset control */
+	mtu->rstc = devm_reset_control_get_optional_shared(pdev->dev.parent, NULL);
+
+	if (IS_ERR(mtu->rstc)) {
+		dev_err(&mtu->pdev->dev, "cannot get reset control\n");
+		return PTR_ERR(mtu->rstc);
+	}
+	ret = reset_control_deassert(mtu->rstc);
+	if (ret < 0)
+		goto err_rstc_assert;
+
 	/* Get hold of clock. */
 	mtu->clk = ddata->clk;
 	if (IS_ERR(mtu->clk)) {
@@ -462,6 +480,8 @@ err_unmap:
 	clk_unprepare(mtu->clk);
 err_clk_put:
 	clk_put(mtu->clk);
+err_rstc_assert:
+	reset_control_assert(mtu->rstc);
 	return ret;
 }
 
