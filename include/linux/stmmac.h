c0b132a7fe9c0f2d0db28749001588e5ba13001a
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-11-03 12:35:54 +0100

    net: stmmac: provide stmmac_pltfr_find_clk()
    [ Upstream commit de64872019492e4a84ee053e635cdf8f63f853d2 ]
    
    Provide a generic way to find a clock in the bulk data.
    
    Reviewed-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Tested-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    Link: https://patch.msgid.link/E1u2QO4-001Rp2-Dy@rmk-PC.armlinux.org.uk
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    [john.madieu:
     - Added clks and num_clks fields in plat_stmmacenet_data]
    Signed-off-by: John Madieu <john.madieu.xa@bp.renesas.com>
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -246,6 +246,8 @@ struct plat_stmmacenet_data {
 	struct clk *clk_tx_i;		/* clk_tx_i to MAC core */
 	unsigned int clk_ptp_rate;
 	unsigned int clk_ref_rate;
+	struct clk_bulk_data *clks;
+	int num_clks;
 	unsigned int mult_fact_100ns;
 	s32 ptp_max_adj;
 	u32 cdc_error_adj;
