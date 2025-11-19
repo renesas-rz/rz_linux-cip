ceaa7648f888d4bae36c31b2a31d3d2f8725def7
Minh Nguyen <minh.nguyen.zj@renesas.com>
2025-11-19 09:34:44 +0700

    reset: rzv2h-usb2phy: Add support for VBUS mux controller registration
    The RZ/V2H USB2 PHY requires control of the VBUS selection line
    (VBENCTL) through a mux controller described in the device tree as
    "mux-controller". This change adds support for registering
    vbus-sel-mux auxiliary driver during probe.
    
    This enables proper management of USB2.0 VBUS source selection on
    platforms using the RZ/V2H SoC.
    
    Signed-off-by: Tommaso Merciai <tommaso.merciai.xr@bp.renesas.com>
    Signed-off-by: Minh Nguyen <minh.nguyen.zj@renesas.com>

--- drivers/reset/reset-rzv2h-usb2phy.c
+++ drivers/reset/reset-rzv2h-usb2phy.c
@@ -15,6 +15,11 @@
 #include <linux/pm_runtime.h>
 #include <linux/reset.h>
 #include <linux/reset-controller.h>
+#include <linux/auxiliary_bus.h>
+#include <linux/idr.h>
+#include <linux/reset/reset_rzv2h_usb2phy.h>
+
+static DEFINE_IDA(auxiliary_ids);
 
 struct rzv2h_usb2phy_regval {
 	u16 reg;
@@ -106,6 +111,41 @@ static int rzv2h_usb2phy_reset_of_xlate(struct reset_controller_dev *rcdev,
 	return 0;
 }
 
+static void rzv2h_usb2phy_reset_ida_free(void *data)
+{
+	struct auxiliary_device *adev = data;
+
+	ida_free(&auxiliary_ids, adev->id);
+}
+
+static int rzv2h_usb2phy_reset_mux_register(struct device *dev,
+					    void __iomem *base,
+					    const char *mux_name)
+{
+	struct reset_rzv2h_usb2phy_adev *rdev;
+	struct auxiliary_device *adev;
+	int id;
+
+	rdev = devm_kzalloc(dev, sizeof(*rdev), GFP_KERNEL);
+	if (!rdev)
+		return -ENOMEM;
+
+	rdev->base = base;
+
+	id = ida_alloc(&auxiliary_ids, GFP_KERNEL);
+	if (id < 0)
+		return id;
+
+	adev = __devm_auxiliary_device_create(dev, dev->driver->name,
+					      mux_name, rdev, id);
+	if (!adev) {
+		ida_free(&auxiliary_ids, id);
+		return -ENOMEM;
+	}
+
+	return devm_add_action_or_reset(dev, rzv2h_usb2phy_reset_ida_free, adev);
+}
+
 static void rzv2h_usb2phy_reset_pm_runtime_put(void *data)
 {
 	pm_runtime_put(data);
@@ -176,6 +216,10 @@ static int rzv2h_usb2phy_reset_probe(struct platform_device *pdev)
 			      "devm_reset_controller_register() failed");
 		goto out_assert_rst;
 	}
+
+	error = rzv2h_usb2phy_reset_mux_register(dev, priv->base, "vbenctl");
+	if (error)
+		return dev_err_probe(dev, error, "could not register aux mux\n");
 
 	return 0;
 
