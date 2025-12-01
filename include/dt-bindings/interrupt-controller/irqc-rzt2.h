4b003415b3d0a6a7e83cb99e56ea31ff9e4c1449
Huy Nguyen <huy.nguyen.wh@renesas.com>
2025-12-01 10:42:54 +0700

    dt-bindings: irqc-rzt2: extend the RZT2_IRQ macros
    RZ/T2N can support 16 IRQC interrupts.
    
    Signed-off-by: Huy Nguyen <huy.nguyen.wh@renesas.com>
    Signed-off-by: Anh Ly <anh.ly.yb@renesas.com>

---
+++ include/dt-bindings/interrupt-controller/irqc-rzt2.h
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * This header provides constants for Renesas RZ/T2 family IRQC bindings.
+ *
+ * Copyright (C) 2024 Renesas Electronics Corp.
+ *
+ */
+
+#ifndef __DT_BINDINGS_IRQC_RZT2_H
+#define __DT_BINDINGS_IRQC_RZT2_H
+
+/*
+ * - RZ/T2: IRQ0-15 map to SPI16-31
+ */
+
+#define RZT2_IRQ0	0
+#define RZT2_IRQ1	1
+#define RZT2_IRQ2	2
+#define RZT2_IRQ3	3
+#define RZT2_IRQ4	4
+#define RZT2_IRQ5	5
+#define RZT2_IRQ6	6
+#define RZT2_IRQ7	7
+#define RZT2_IRQ8	8
+#define RZT2_IRQ9	9
+#define RZT2_IRQ10	10
+#define RZT2_IRQ11	11
+#define RZT2_IRQ12	12
+#define RZT2_IRQ13	13
+#define RZT2_IRQ14	14
+#define RZT2_IRQ15	15
+
+#endif /* __DT_BINDINGS_IRQC_RZT2_H */
