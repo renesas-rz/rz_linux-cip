0f3bd6d7a6de65c237c0f83c1b4fbea56d6a442c
Hiep Pham <hiep.pham.zy@renesas.com>
2025-12-12 02:12:07 +0700

    drm: renesas: rz-du: mipi_dsi: Set DSI divider
    Before the MIPI DSI clock source can be configured, the target divide
    ratio needs to be known.
    
    Refer Link: https://patchwork.kernel.org/project/linux-renesas-soc/patch/20251124131003.992554-3-chris.brandt@renesas.com/
    
    Signed-off-by: Chris Brandt <chris.brandt@renesas.com>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Tested-by: Biju Das <biju.das.jz@bp.renesas.com>
    Signed-off-by: Hiep Pham <hiep.pham.zy@renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2022 Renesas Electronics Corporation
  */
 #include <linux/clk.h>
+#include <linux/clk/renesas.h>
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/iopoll.h>
@@ -607,6 +608,7 @@ static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
 				      struct mipi_dsi_device *device)
 {
 	struct rzg2l_mipi_dsi *dsi = host_to_rzg2l_mipi_dsi(host);
+	int bpp;
 	int ret;
 
 	if (device->lanes > dsi->num_data_lanes) {
@@ -616,7 +618,8 @@ static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
 		return -EINVAL;
 	}
 
-	switch (mipi_dsi_pixel_format_to_bpp(device->format)) {
+	bpp = mipi_dsi_pixel_format_to_bpp(device->format);
+	switch (bpp) {
 	case 24:
 	case 18:
 		break;
@@ -638,6 +641,18 @@ static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
 	}
 
 	drm_bridge_add(&dsi->bridge);
+
+	/*
+	 * Report the required division ratio setting for the MIPI clock dividers.
+	 *
+	 * vclk * bpp = hsclk * 8 * num_lanes
+	 *
+	 * vclk * DSI_AB_divider = hsclk * 16
+	 *
+	 *   which simplifies to...
+	 * DSI_AB_divider = bpp * 2 / num_lanes
+	 */
+	rzg2l_cpg_dsi_div_set_divider(bpp * 2 / dsi->lanes, PLL5_TARGET_DSI);
 
 	return 0;
 }
