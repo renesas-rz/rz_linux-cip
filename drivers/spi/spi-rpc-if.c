c39cbd6af461024cd749505d9648b0ad0fab0afd
Au Dang <au.dang.jx@renesas.com>
2025-12-18 11:22:08 +0700

    spi: rpc-if: switch to use devm_spi_alloc_host()
    Switch to use modern name function devm_spi_alloc_host().
    No functional changed.
    
    Signed-off-by: Yang Yingliang <yangyingliang@huawei.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/r/20230816094013.1275068-15-yangyingliang@huawei.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>

--- drivers/spi/spi-rpc-if.c
+++ drivers/spi/spi-rpc-if.c
@@ -148,7 +148,7 @@ static int rpcif_spi_probe(struct platform_device *pdev)
 	struct rpcif *rpc;
 	int error;
 
-	ctlr = devm_spi_alloc_master(&pdev->dev, sizeof(*rpc));
+	ctlr = devm_spi_alloc_host(&pdev->dev, sizeof(*rpc));
 	if (!ctlr)
 		return -ENOMEM;
 
