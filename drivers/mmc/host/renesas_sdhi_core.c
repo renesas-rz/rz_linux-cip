b32ed410c84455d89c87e9d115f3a5fbda8bf42d
Hoa Huynh <hoa.huynh.ud@renesas.com>
2025-12-18 17:15:28 +0700

    mmc: host: renesas_sdhi_core: add delay during voltage switching for UHS mode
    Some SD cards experience a timeout when switching
    from HS mode to UHS mode. Add a delay in the voltage
    switching path to ensure the voltage has fully switched
    to 1.8V before entering UHS mode
    
    Signed-off-by: Hoa Huynh <hoa.huynh.ud@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -279,6 +279,7 @@ static int renesas_sdhi_start_signal_voltage_switch(struct mmc_host *mmc,
 			MMC_SIGNAL_VOLTAGE_330 ? 0 : -EINVAL;
 
 	ret = mmc_regulator_set_vqmmc(host->mmc, ios);
+	mdelay(2);
 	if (ret < 0)
 		return ret;
 
