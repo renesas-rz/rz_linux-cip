bab665f2bf97257df98771307c5e1c083fbffd90
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2025-12-19 14:34:58 +0700

    mmc: renesas_sdhi: Deassert the reset signal on probe
    Deassert the reset signal of the SDHI controller during probe to avoid
    relying on the previous bootloaders. Without deasserting the reset signal,
    the SDHI controller will not function.
    
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
    Signed-off-by: Khai Nguyen <khai.nguyen.wx@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -1184,15 +1184,15 @@ int renesas_sdhi_probe(struct platform_device *pdev,
 	if (IS_ERR(priv->clk_cd))
 		return dev_err_probe(&pdev->dev, PTR_ERR(priv->clk_cd), "cannot get cd clock");
 
-	priv->rstc = devm_reset_control_get_optional_exclusive(&pdev->dev, NULL);
+	priv->rstc = devm_reset_control_get_optional_exclusive_deasserted(&pdev->dev, NULL);
 	if (IS_ERR(priv->rstc))
 		return PTR_ERR(priv->rstc);
 
-	priv->rstc_axim = devm_reset_control_get_optional_exclusive(&pdev->dev, "axim");
+	priv->rstc_axim = devm_reset_control_get_optional_exclusive_deasserted(&pdev->dev, "axim");
 	if (IS_ERR(priv->rstc_axim))
 		return PTR_ERR(priv->rstc_axim);
 
-	priv->rstc_axis = devm_reset_control_get_optional_exclusive(&pdev->dev, "axis");
+	priv->rstc_axis = devm_reset_control_get_optional_exclusive_deasserted(&pdev->dev, "axis");
 	if (IS_ERR(priv->rstc_axis))
 		return PTR_ERR(priv->rstc_axis);
 
