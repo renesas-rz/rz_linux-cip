ef9779c225829f19b8920930f6e63bb32de9e850
Hiep Pham <hiep.pham.zy@renesas.com>
2026-01-02 10:55:49 +0700

    clk: renesas: rzv2h-cpg: Add support S2R
    This patch adds PM suspend/resume callbacks.
    
    Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>
    Signed-off-by: Hiep Pham <hiep.pham.zy@renesas.com>

--- drivers/clk/renesas/r9a09g057-cpg.c
+++ drivers/clk/renesas/r9a09g057-cpg.c
@@ -115,7 +115,7 @@ static const char * const smux2_gbe1_txclk[] = { ".plleth_gbe1", "et1_txclk" };
 static const char * const smux2_xspi_clk0[] = { ".pllcm33_div3", ".pllcm33_div4" };
 static const char * const smux2_xspi_clk1[] = { ".smux2_xspi_clk0", ".pllcm33_div5" };
 
-static const struct cpg_core_clk r9a09g057_core_clks[] __initconst = {
+static const struct cpg_core_clk r9a09g057_core_clks[] = {
 	/* External Clock Inputs */
 	DEF_INPUT("audio_extal", CLK_AUDIO_EXTAL),
 	DEF_INPUT("rtxin", CLK_RTXIN),
@@ -193,7 +193,7 @@ static const struct cpg_core_clk r9a09g057_core_clks[] __initconst = {
 			     FIXED_MOD_CONF_XSPI),
 };
 
-static const struct rzv2h_mod_clk r9a09g057_mod_clks[] __initconst = {
+static const struct rzv2h_mod_clk r9a09g057_mod_clks[] = {
 	DEF_MOD("dmac_0_aclk",			CLK_PLLCM33_GEAR, 0, 0, 0, 0,
 						BUS_MSTOP(5, BIT(9))),
 	DEF_MOD("dmac_1_aclk",			CLK_PLLDTY_ACPU_DIV2, 0, 1, 0, 1,
@@ -358,7 +358,7 @@ static const struct rzv2h_mod_clk r9a09g057_mod_clks[] __initconst = {
 						BUS_MSTOP(3, BIT(4))),
 };
 
-static const struct rzv2h_reset r9a09g057_resets[] __initconst = {
+static const struct rzv2h_reset r9a09g057_resets[] = {
 	DEF_RST(3, 0, 1, 1),		/* SYS_0_PRESETN */
 	DEF_RST(3, 1, 1, 2),		/* DMAC_0_ARESETN */
 	DEF_RST(3, 2, 1, 3),		/* DMAC_1_ARESETN */
@@ -420,7 +420,7 @@ static const struct rzv2h_reset r9a09g057_resets[] __initconst = {
 	DEF_RST(13, 15, 6, 16),		/* GPU_0_ACE_RESETN */
 };
 
-const struct rzv2h_cpg_info r9a09g057_cpg_info __initconst = {
+const struct rzv2h_cpg_info r9a09g057_cpg_info  = {
 	/* Core Clocks */
 	.core_clks = r9a09g057_core_clks,
 	.num_core_clks = ARRAY_SIZE(r9a09g057_core_clks),
