8a85c76c96c6f46af2b02502d5c350271243c2cb
Thang Dinh <thang.dinh.fv@renesas.com>
2026-01-12 13:54:59 +0700

    i3c: master: Enhance i3c_bus_type visibility for device searching & event monitoring
    Improve the visibility of i3c_bus_type to facilitate searching for
    i3c devices attached to the i3c bus. Enable other drivers to use
    bus_register_notifier to monitor i3c bus device events.
    
    Signed-off-by: Bhoomik Gupta <bhoomik.gupta@nxp.com>
    Signed-off-by: Thang Dinh <thang.dinh.fv@renesas.com>

--- include/linux/i3c/master.h
+++ include/linux/i3c/master.h
@@ -26,6 +26,7 @@ struct i3c_master_controller;
 struct i3c_bus;
 struct i2c_device;
 struct i3c_device;
+extern struct bus_type i3c_bus_type;
 
 /**
  * struct i3c_i2c_dev_desc - Common part of the I3C/I2C device descriptor
