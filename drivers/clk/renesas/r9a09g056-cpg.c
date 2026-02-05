cb84f1a18f694c436df1ff9e0e6244ea85b10c7c
Hien Dang <hien.dang.xf@renesas.com>
2026-02-05 17:49:32 +0700

    clk: renesas: r9a09g056-cpg: Add clock and reset entries for RTC
    Add clock and reset entries for RTC module on the RZ/V2N SoC.
    
    Signed-off-by: Hien Dang <hien.dang.xf@renesas.com>

--- drivers/clk/renesas/r9a09g056-cpg.c
+++ drivers/clk/renesas/r9a09g056-cpg.c
@@ -299,6 +299,8 @@ static const struct rzv2h_mod_clk r9a09g056_mod_clks[] = {
 						BUS_MSTOP(5, BIT(13))),
 	DEF_MOD("wdt_3_clk_loco",		CLK_QEXTAL, 5, 2, 2, 18,
 						BUS_MSTOP(5, BIT(13))),
+	DEF_MOD("rtc_clk_rtc",			CLK_PLLCM33_DIV16, 5, 3, 2, 19,
+						BUS_MSTOP(3, BIT(11) | BIT(12))),
 	DEF_MOD("rspi_0_pclk",			CLK_PLLCLN_DIV8, 5, 4, 2, 20,
 						BUS_MSTOP(11, BIT(0))),
 	DEF_MOD("rspi_0_pclk_sfr",		CLK_PLLCLN_DIV8, 5, 5, 2, 21,
@@ -487,6 +489,8 @@ static const struct rzv2h_reset r9a09g056_resets[] = {
 	DEF_RST(7, 6, 3, 7),		/* WDT_1_RESET */
 	DEF_RST(7, 7, 3, 8),		/* WDT_2_RESET */
 	DEF_RST(7, 8, 3, 9),		/* WDT_3_RESET */
+	DEF_RST(7, 9, 3, 10),		/* RTC_RST_RTC */
+	DEF_RST(7, 10, 3, 11),		/* RTC_RST_RTC_V */
 	DEF_RST(7, 11, 3, 12),		/* RSPI_0_PRESETN */
 	DEF_RST(7, 12, 3, 13),		/* RSPI_0_TRESETN */
 	DEF_RST(7, 13, 3, 14),		/* RSPI_1_PRESETN */
