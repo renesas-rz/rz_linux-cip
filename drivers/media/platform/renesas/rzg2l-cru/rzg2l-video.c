60be147a0862be5c28f7c92ffd8459ac12b82792
Tranh Ha <tranh.ha.xb@renesas.com>
2026-03-24 11:19:31 +0700

    media: rzg2l-cru: set auto recovery mode at the beginning of streaming
    We can set auto recovery mode to clear all FIFOs and resume transmission
    when AXI-Bus is congested at the beginning of streaming.
    No need to set it in interrupt handler.
    
    Signed-off-by: Tranh Ha <tranh.ha.xb@renesas.com>
    Signed-off-by: Hien Huynh <hien.huynh.px@renesas.com>
    Signed-off-by: Khai Nguyen <khai.nguyen.wx@renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -39,7 +39,6 @@ struct rzg2l_cru_buffer {
 	struct list_head list;
 };
 
-static int prev_slot[RZG2L_CRU_MAX];
 static void rzg2l_cru_initialize_axi(struct rzg2l_cru_dev *cru);
 
 #define to_buf_list(vb2_buffer) \
@@ -333,6 +332,13 @@ static void rzg2l_cru_initialize_axi(struct rzg2l_cru_dev *cru)
 	amnaxiattr = rzg2l_cru_read(cru, AMnAXIATTR) & ~AMnAXIATTR_AXILEN_MASK;
 	amnaxiattr |= AMnAXIATTR_AXILEN;
 	rzg2l_cru_write(cru, AMnAXIATTR, amnaxiattr);
+
+       /*
+        * AXI-Bus congestion maybe occurred.
+        * Set auto recovery mode to clear all FIFOs
+        * and resume transmission.
+        */
+	rzg2l_cru_write(cru, AMnFIFO, 0);
 }
 
 void rzg3e_cru_csi2_setup(struct rzg2l_cru_dev *cru,
@@ -903,23 +909,6 @@ irqreturn_t rzg2l_cru_irq(int irq, void *data)
 		cru->state = RZG2L_CRU_DMA_RUNNING;
 	}
 
-	if ((cru->num_buf == 1) || (slot != prev_slot[cru->id])) {
-		/* Update value of previous memory bank slot */
-		prev_slot[cru->id] = slot;
-	} else {
-		/*
-		 * AXI-Bus congestion maybe occurred.
-		 * Set auto recovery mode to clear all FIFOs
-		 * and resume transmission.
-		 */
-		rzg2l_cru_write(cru, AMnFIFO, 0);
-		prev_slot[cru->id] = -1;
-
-		dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
-			cru->sequence, cru->id);
-		goto done;
-	}
-
 	/* Capture frame */
 	if (cru->queue_buf[slot]) {
 		cru->queue_buf[slot]->field = cru->format.field;
@@ -1032,23 +1021,6 @@ irqreturn_t rzg3e_cru_irq(int irq, void *data)
 			cru->state = RZG2L_CRU_DMA_RUNNING;
 		}
 
-		if ((cru->num_buf == 1) || (slot != prev_slot[cru->id])) {
-			/* Update value of previous memory bank slot */
-			prev_slot[cru->id] = slot;
-		} else {
-			/*
-			 * AXI-Bus congestion maybe occurred.
-			 * Set auto recovery mode to clear all FIFOs
-			 * and resume transmission.
-			 */
-			rzg2l_cru_write(cru, AMnFIFO, 0);
-			prev_slot[cru->id] = -1;
-
-			dev_dbg(cru->dev, "Dropping frame %u with CRU channel %d\n",
-				cru->sequence, cru->id);
-			return IRQ_HANDLED;
-		}
-
 		/* Capture frame */
 		if (cru->queue_buf[slot]) {
 			struct vb2_v4l2_buffer *buf = cru->queue_buf[slot];
@@ -1122,9 +1094,6 @@ static int rzg2l_cru_start_streaming_vq(struct vb2_queue *vq, unsigned int count
 	}
 
 	cru->state = RZG2L_CRU_DMA_STARTING;
-
-	/* Initialize value of previous memory bank slot before streaming */
-	prev_slot[cru->id] = -1;
 
 	dev_dbg(cru->dev, "Starting to capture\n");
 	return 0;
