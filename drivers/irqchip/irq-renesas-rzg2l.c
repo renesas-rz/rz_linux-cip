2be83aa421174870edacbc9e285650bfb5be81a7
Biju Das <biju.das.jz@bp.renesas.com>
2026-03-28 10:33:19 +0000

    irqchip/renesas-rzg2l: Replace raw_spin_{lock,unlock} with guard() in rzg2l_irq_set_type()
    Simplify the locking logic in rzg2l_irq_set_type() by using guard(),
    eliminating the need for an explicit unlock call.
    
    While at it, add the missing cleanup.h header file.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- drivers/irqchip/irq-renesas-rzg2l.c
+++ drivers/irqchip/irq-renesas-rzg2l.c
@@ -8,6 +8,7 @@
  */
 
 #include <linux/bitfield.h>
+#include <linux/cleanup.h>
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/io.h>
@@ -373,14 +374,13 @@ static int rzg2l_irq_set_type(struct irq_data *d, unsigned int type)
 		return -EINVAL;
 	}
 
-	raw_spin_lock(&priv->lock);
+	guard(raw_spinlock)(&priv->lock);
 	tmp = readl_relaxed(priv->base + IITSR);
 	tmp &= ~IITSR_IITSEL_MASK(iitseln);
 	tmp |= IITSR_IITSEL(iitseln, sense);
 	if (clear_irq_int)
 		rzg2l_clear_irq_int(priv, hwirq);
 	writel_relaxed(tmp, priv->base + IITSR);
-	raw_spin_unlock(&priv->lock);
 
 	return 0;
 }
