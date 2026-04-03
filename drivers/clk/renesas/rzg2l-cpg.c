089edf5b0cdf5afd3b132424250126a6e84278b7
Biju Das <biju.das.jz@bp.renesas.com>
2026-04-03 11:15:12 +0700

    clk: renesas: rzg2l-cpg: Add helper for mod clock enable/disable
    commit 867fb0bc60602cb3c2458fcd25c841650d37563f upstream.
    
    Refactor rzg2l_mod_clock_endisable() by extracting its logic into a new
    helper function rzg2l_mod_clock_endisable_helper(), which accepts an
    additional set_mstop_state boolean parameter.  This allows callers to
    control whether the module stop state is updated alongside the clock
    enable/disable operation.  No functional change for existing callers.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://patch.msgid.link/20260324114329.268249-5-biju.das.jz@bp.renesas.com
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Nghia Nguyen <nghia.nguyen.xm@renesas.com>

--- drivers/clk/renesas/rzg2l-cpg.c
+++ drivers/clk/renesas/rzg2l-cpg.c
@@ -1726,7 +1726,8 @@ static int rzg2l_mod_clock_mstop_show(struct seq_file *s, void *what)
 }
 DEFINE_SHOW_ATTRIBUTE(rzg2l_mod_clock_mstop);
 
-static int rzg2l_mod_clock_endisable(struct clk_hw *hw, bool enable)
+static int rzg2l_mod_clock_endisable_helper(struct clk_hw *hw, bool enable,
+					    bool set_mstop_state)
 {
 	struct mstp_clock *clock = to_mod_clock(hw);
 	struct rzg2l_cpg_priv *priv = clock->priv;
@@ -1751,10 +1752,12 @@ static int rzg2l_mod_clock_endisable(struct clk_hw *hw, bool enable)
 	scoped_guard(spinlock_irqsave, &priv->rmw_lock) {
 		if (enable) {
 			writel(value, priv->base + CLK_ON_R(reg));
-			rzg2l_mod_clock_module_set_state(clock, false);
+			if (set_mstop_state)
+				rzg2l_mod_clock_module_set_state(clock, false);
 		} else {
 			rzg2l_mod_clock_module_set_state(clock, true);
-			writel(value, priv->base + CLK_ON_R(reg));
+			if (set_mstop_state)
+				writel(value, priv->base + CLK_ON_R(reg));
 		}
 	}
 
@@ -1771,6 +1774,11 @@ static int rzg2l_mod_clock_endisable(struct clk_hw *hw, bool enable)
 			priv->base + CLK_ON_R(reg));
 
 	return error;
+}
+
+static int rzg2l_mod_clock_endisable(struct clk_hw *hw, bool enable)
+{
+	return rzg2l_mod_clock_endisable_helper(hw, enable, true);
 }
 
 static int rzg2l_mod_clock_enable(struct clk_hw *hw)
