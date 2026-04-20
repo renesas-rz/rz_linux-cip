3903e226358fc18a78830f7cbea9967850011b74
Long Luu <long.luu.ur@renesas.com>
2026-04-20 13:22:07 +0700

    memory: renesas-rpc-if: Fix duplicate device name on multi-instance platforms
    On platforms with multiple xSPI instances, the driver fails to probe
    additional instances due to duplicate sysfs entries:
    
      [   86.878242] sysfs: cannot create duplicate filename '/bus/platform/devices/rpc-if-spi'
    
    This occurs because platform_device_alloc() uses pdev->id for the device
    ID, which may be PLATFORM_DEVID_NONE (-1) for multiple instances, causing
    all instances to attempt registration with the same name.
    
    Fix this by using PLATFORM_DEVID_AUTO instead, which automatically assigns
    unique IDs to each device instance, allowing multiple xSPI controllers to
    coexist without naming conflicts.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Long Luu <long.luu.ur@renesas.com>
    Signed-off-by: Anh Ly <anh.ly.yb@renesas.com>

--- drivers/memory/renesas-rpc-if.c
+++ drivers/memory/renesas-rpc-if.c
@@ -1038,7 +1038,7 @@ static int rpcif_probe(struct platform_device *pdev)
 					     "cannot get enabled spi clk\n");
 	}
 
-	vdev = platform_device_alloc(name, pdev->id);
+	vdev = platform_device_alloc(name, PLATFORM_DEVID_AUTO);
 	if (!vdev)
 		return -ENOMEM;
 	vdev->dev.parent = dev;
