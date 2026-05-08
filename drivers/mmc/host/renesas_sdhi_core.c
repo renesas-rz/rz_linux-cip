d183387fbac57a50ac23065e701de81da12a0a99
Hiep Pham <hiep.pham.zy@renesas.com>
2026-05-08 10:36:02 +0700

    mmc: renesas_sdhi: add delay between tuning cmds
    This commit update fix issue SD cards can not detect normally with
    SDR104 mode (error tuning -5) for all board (G2HMNE/L Series, V2L, Five)
    
    But this accidently apply for eMMC too and cause delayed boot sometimes.
    So avoid that (adding delay) in eMMC case.
    
    Reference link:
    https://patchwork.kernel.org/project/linux-renesas-soc/patch/20230531070049.11806-1-wsa+renesas@sang-engineering.com/
    Some SD cards failed to tune SDR104 occasionally. Adding a delay between
    tuning commands makes the cards work although it is not yet known why.
    It seems to be host dependent, only a few need such a delay (I found AMD
    SDHCI and Freescale eSDHC so far). 25us is not enough, 50us never failed
    up to now, so let's double that value until we know what is going on.
    Extra thank you to Adam for sending me a problematic SD card to test.
    
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Au Dang <au.dang.jx@renesas.com>
    Signed-off-by: Hiep Pham <hiep.pham.zy@renesas.com>

--- drivers/mmc/host/renesas_sdhi_core.c
+++ drivers/mmc/host/renesas_sdhi_core.c
@@ -733,6 +733,10 @@ static int renesas_sdhi_execute_tuning(struct mmc_host *mmc, u32 opcode)
 
 		if (cmd_error)
 			mmc_send_abort_tuning(mmc, opcode);
+
+		/* FIXME: Needed for some SD cards. The reason is not known yet */
+		if (!(host->mmc->caps & MMC_CAP_NONREMOVABLE))
+			usleep_range(1000, 1250);
 	}
 
 	ret = renesas_sdhi_select_tuning(host);
