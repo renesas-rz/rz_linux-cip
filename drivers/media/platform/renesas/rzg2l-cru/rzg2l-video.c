23e285960a8180dbbad1ab0bec0196aa584d6406
Jacopo Mondi <jacopo.mondi+renesas@ideasonboard.com>
2026-07-16 14:26:51 +0700

    media: rzg2l-cru: Simplify irq return value handling
    The rzg2l_cru_irq() irq handler uses a local variable to store the
    handler return value.
    
    Simplify it by using IRQ_NONE and IRQ_HANDLED.
    
    Signed-off-by: Jacopo Mondi <jacopo.mondi+renesas@ideasonboard.com>
    Reviewed-by: Tommaso Merciai <tommaso.merciai.xr@bp.renesas.com>
    Reviewed-by: Daniel Scally <dan.scally@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -932,18 +932,15 @@ static void rzg2l_cru_stop_streaming(struct rzg2l_cru_dev *cru)
 irqreturn_t rzg2l_cru_irq(int irq, void *data)
 {
 	struct rzg2l_cru_dev *cru = data;
-	unsigned int handled = 0;
 	u32 irq_status;
 	u32 amnmbs;
 	int slot;
 
 	irq_status = rzg2l_cru_read(cru, CRUnINTS);
 	if (!irq_status)
-		return IRQ_RETVAL(handled);
+		return IRQ_NONE;
 
 	dev_dbg(cru->dev, "CRUnINTS 0x%x\n", irq_status);
-
-	handled = 1;
 
 	rzg2l_cru_write(cru, CRUnINTS, rzg2l_cru_read(cru, CRUnINTS));
 
@@ -986,7 +983,7 @@ irqreturn_t rzg2l_cru_irq(int irq, void *data)
 	/* Prepare for next frame */
 	rzg2l_cru_fill_hw_slot(cru, slot);
 
-	return IRQ_RETVAL(handled);
+	return IRQ_HANDLED;
 }
 
 irqreturn_t rzg3e_cru_irq(int irq, void *data)
